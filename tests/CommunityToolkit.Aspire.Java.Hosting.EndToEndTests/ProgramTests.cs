164301961dff3029e4a6bbf95bf8895723b8d837
Aaron Powell <me@aaron-powell.com>
2024-07-31 00:39:24 +0000

    Turning up the logging

--- tests/CommunityToolkit.Aspire.Java.Hosting.EndToEndTests/ProgramTests.cs
+++ tests/CommunityToolkit.Aspire.Java.Hosting.EndToEndTests/ProgramTests.cs
@@ -1,16 +1,27 @@
 using System.Net;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
 
 namespace CommunityToolkit.Aspire.Java.Hosting.EndToEndTests;
 
-public class ProgramTests
+public class ProgramTests(ITestOutputHelper testOutput)
 {
     [Fact]
     public async Task Given_Container_Resource_When_Invoked_Then_Root_Returns_OK()
     {
         // Arrange
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.CommunityToolkit_Aspire_Java_AppHost>();
+
+        appHost.Services
+            .AddLogging(builder =>
+            {
+                builder.AddXUnit(testOutput);
+                builder.SetMinimumLevel(LogLevel.Trace);
+            })
+            .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler());
+
         await using var app = await appHost.BuildAsync();
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
         await app.StartAsync();
@@ -31,6 +42,15 @@ public class ProgramTests
     {
         // Arrange
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.CommunityToolkit_Aspire_Java_AppHost>();
+
+        appHost.Services
+            .AddLogging(builder =>
+            {
+                builder.AddXUnit(testOutput);
+                builder.SetMinimumLevel(LogLevel.Trace);
+            })
+            .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler());
+
         await using var app = await appHost.BuildAsync();
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
         await app.StartAsync();
