3eb45bd5febebcd3893c020df1cfc91d51dfa21b
Aaron Powell <me@aaron-powell.com>
2024-07-31 11:36:48 +1000

    Enable Java tests (#7)
    * Enabling java tests and trying to push diagnostic logs
    
    * fixing path
    
    * Fixing the project that the tests were starting up
    
    * seems it was set wrong
    
    * Using new WaitForResource method to delay test execution
    
    * Turning up the logging
    
    * Trying something out
    
    * Not having the StandardResilienceHandler is the problem

---
+++ tests/CommunityToolkit.Aspire.Java.Hosting.EndToEndTests/ProgramTests.cs
@@ -0,0 +1,68 @@
+using System.Net;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace CommunityToolkit.Aspire.Java.Hosting.EndToEndTests;
+
+public class ProgramTests(ITestOutputHelper testOutput)
+{
+    [Fact]
+    public async Task Given_Container_Resource_When_Invoked_Then_Root_Returns_OK()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.CommunityToolkit_Aspire_Java_AppHost>();
+
+        appHost.Services
+            .AddLogging(builder =>
+            {
+                builder.AddXUnit(testOutput);
+                builder.SetMinimumLevel(LogLevel.Trace);
+            })
+            .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler());
+
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("containerapp");
+
+        await resourceNotificationService.WaitForResourceAsync("containerapp", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        var response = await httpClient.GetAsync("/");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task Given_Executable_Resource_When_Invoked_Then_Root_Returns_OK()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.CommunityToolkit_Aspire_Java_AppHost>();
+
+        appHost.Services
+            .AddLogging(builder =>
+            {
+                builder.AddXUnit(testOutput);
+                builder.SetMinimumLevel(LogLevel.Trace);
+            })
+            .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler());
+
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("executableapp");
+
+        await resourceNotificationService.WaitForResourceAsync("executableapp", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        var response = await httpClient.GetAsync("/");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
