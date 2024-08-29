e55815644aec70632ed9c48afef45bc531b66c2e
Aaron Powell <me@aaron-powell.com>
2024-08-29 06:07:37 +0000

    Debug logging

--- tests/CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps.Tests/SwaHostingComponentTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps.Tests/SwaHostingComponentTests.cs
@@ -1,5 +1,7 @@
 using CommunityToolkit.Aspire.Testing;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Net;
 using System.Net.Http.Json;
 
@@ -10,11 +12,17 @@ public class SwaHostingComponentTests(AspireIntegrationTestFixture<Projects.Comm
     [Fact]
     public async Task CanAccessFrontendSuccessfully()
     {
+        var logger = fixture.App.Services.GetRequiredService<ILogger<SwaHostingComponentTests>>();
+        logger.LogInformation("Starting test");
         var httpClient = fixture.CreateHttpClient("swa");
 
+        logger.LogInformation("Waiting for resources to be ready");
         await fixture.ResourceNotificationService.WaitForResourceAsync("swa", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        logger.LogInformation("swa is ready");
         await fixture.ResourceNotificationService.WaitForResourceAsync("web", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        logger.LogInformation("web is ready");
         await fixture.ResourceNotificationService.WaitForResourceAsync("api", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        logger.LogInformation("api is ready");
 
         var response = await httpClient.GetAsync("/");
 
@@ -24,11 +32,17 @@ public class SwaHostingComponentTests(AspireIntegrationTestFixture<Projects.Comm
     [Fact]
     public async Task CanAccessApiSuccessfully()
     {
+        var logger = fixture.App.Services.GetRequiredService<ILogger<SwaHostingComponentTests>>();
+        logger.LogInformation("Starting test");
         var httpClient = fixture.CreateHttpClient("swa");
 
+        logger.LogInformation("Waiting for resources to be ready");
         await fixture.ResourceNotificationService.WaitForResourceAsync("swa", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        logger.LogInformation("swa is ready");
         await fixture.ResourceNotificationService.WaitForResourceAsync("web", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        logger.LogInformation("web is ready");
         await fixture.ResourceNotificationService.WaitForResourceAsync("api", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        logger.LogInformation("api is ready");
 
         var response = await httpClient.GetAsync("/api/weather");
 
