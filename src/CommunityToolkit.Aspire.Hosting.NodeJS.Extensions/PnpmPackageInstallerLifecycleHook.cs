9f683d4e80696406d8215794fb4b6705a4ca04b8
Aaron Powell <me@aaron-powell.com>
2024-09-12 05:01:26 +0000

    Some missing docs

--- src/CommunityToolkit.Aspire.Hosting.NodeJS.Extensions/PnpmPackageInstallerLifecycleHook.cs
+++ src/CommunityToolkit.Aspire.Hosting.NodeJS.Extensions/PnpmPackageInstallerLifecycleHook.cs
@@ -4,6 +4,12 @@ using Aspire.Hosting.Lifecycle;
 
 namespace CommunityToolkit.Aspire.Hosting.NodeJS.Extensions;
 
+/// <summary>
+/// An <see cref="IDistributedApplicationLifecycleHook"/> that installs Node.js packages using the pnpm package manager before the Node.js resource starts.
+/// </summary>
+/// <param name="loggerService">The <see cref="ResourceLoggerService"/> to use for logging.</param>
+/// <param name="notificationService">The <see cref="ResourceNotificationService"/> to use for notifications to Aspire on install progress.</param>
+/// <param name="context">The <see cref="DistributedApplicationExecutionContext"/> to use for determining if the application is in publish mode.</param>
 internal class PnpmPackageInstallerLifecycleHook(
     ResourceLoggerService loggerService,
     ResourceNotificationService notificationService,
@@ -11,6 +17,7 @@ internal class PnpmPackageInstallerLifecycleHook(
 {
     private readonly NodePackageInstaller _installer = new("pnpm", loggerService, notificationService);
 
+    /// <inheritdoc />
     public Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
     {
         if (context.IsPublishMode)
--- src/CommunityToolkit.Aspire.Hosting.NodeJS.Extensions/YarnPackageInstallerLifecycleHook.cs
+++ src/CommunityToolkit.Aspire.Hosting.NodeJS.Extensions/YarnPackageInstallerLifecycleHook.cs
@@ -4,6 +4,12 @@ using Aspire.Hosting.Lifecycle;
 
 namespace CommunityToolkit.Aspire.Hosting.NodeJS.Extensions;
 
+/// <summary>
+/// An <see cref="IDistributedApplicationLifecycleHook"/> that installs Node.js packages using the yarn package manager before the Node.js resource starts.
+/// </summary>
+/// <param name="loggerService">The <see cref="ResourceLoggerService"/> to use for logging.</param>
+/// <param name="notificationService">The <see cref="ResourceNotificationService"/> to use for notifications to Aspire on install progress.</param>
+/// <param name="context">The <see cref="DistributedApplicationExecutionContext"/> to use for determining if the application is in publish mode.</param>
 internal class YarnPackageInstallerLifecycleHook(
     ResourceLoggerService loggerService,
     ResourceNotificationService notificationService,
@@ -11,6 +17,7 @@ internal class YarnPackageInstallerLifecycleHook(
 {
     private readonly NodePackageInstaller _installer = new("yarn", loggerService, notificationService);
 
+    /// <inheritdoc />
     public Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
     {
         if (context.IsPublishMode)
