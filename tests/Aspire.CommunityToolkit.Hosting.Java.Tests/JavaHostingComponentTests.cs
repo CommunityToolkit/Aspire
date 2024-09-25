8253a679c8f6277a1cf7067a38d021fd1c3ad75e
Aaron Powell <me@aaron-powell.com>
2024-09-25 06:05:43 +0000

    Disabling some tests on Windows.
    GitHub Actions only supports Windows containers on their agents, which means that we can't pull certain container images during tests. Guess that sees some tests disabled on Windows...

--- tests/Aspire.CommunityToolkit.Hosting.Java.Tests/JavaHostingComponentTests.cs
+++ tests/Aspire.CommunityToolkit.Hosting.Java.Tests/JavaHostingComponentTests.cs
@@ -1,16 +1,30 @@
 using Aspire.CommunityToolkit.Testing;
 using FluentAssertions;
+using Microsoft.TestUtilities;
 
 namespace Aspire.CommunityToolkit.Hosting.Java.Tests;
 
 #pragma warning disable CTASPIRE001
 public class JavaHostingComponentTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Java_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Java_AppHost>>
 {
-    [Theory]
-    [InlineData("containerapp")]
-    [InlineData("executableapp")]
-    public async Task ResourceWillRespondWithOk(string resourceName)
+    [ConditionalFact]
+    [OSSkipCondition(OperatingSystems.Windows)]
+    public async Task ContainerAppResourceWillRespondWithOk()
     {
+        var resourceName = "containerapp";
+        var httpClient = fixture.CreateHttpClient(resourceName);
+
+        await fixture.App.WaitForTextAsync("Started SpringMavenApplication", resourceName).WaitAsync(TimeSpan.FromMinutes(5));
+
+        var response = await httpClient.GetAsync("/");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task ExecutableAppResourceWillRespondWithOk()
+    {
+        var resourceName = "executableapp";
         var httpClient = fixture.CreateHttpClient(resourceName);
 
         await fixture.App.WaitForTextAsync("Started SpringMavenApplication", resourceName).WaitAsync(TimeSpan.FromMinutes(5));
--- tests/Aspire.CommunityToolkit.Hosting.Ollama.Tests/AppHostTests.cs
+++ tests/Aspire.CommunityToolkit.Hosting.Ollama.Tests/AppHostTests.cs
@@ -1,11 +1,13 @@
 using Aspire.CommunityToolkit.Testing;
 using FluentAssertions;
+using Microsoft.TestUtilities;
 
 namespace Aspire.CommunityToolkit.Hosting.Ollama.Tests;
 
 public class AppHostTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Ollama_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Ollama_AppHost>>
 {
-    [Fact]
+    [ConditionalFact]
+    [OSSkipCondition(OperatingSystems.Windows)]
     public async Task ResourceStartsAndRespondsOk()
     {
         await fixture.ResourceNotificationService.WaitForResourceAsync("ollama", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
