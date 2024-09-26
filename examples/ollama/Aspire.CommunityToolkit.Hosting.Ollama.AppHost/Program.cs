fe851826d932e72aa11e9fb581a361de0b9abcdc
Aaron Powell <me@aaron-powell.com>
2024-09-26 21:56:13 +1000

    experimenting with running multiple modes
    Fixing conditional tests

--- examples/ollama/Aspire.CommunityToolkit.Hosting.Ollama.AppHost/Program.cs
+++ examples/ollama/Aspire.CommunityToolkit.Hosting.Ollama.AppHost/Program.cs
@@ -2,6 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var ollama = builder.AddOllama("ollama", port: null)
     .AddModel("phi3")
+    .AddModel("gemma2:2b")
     .WithDefaultModel("phi3");
 
 builder.AddProject<Projects.Aspire_CommunityToolkit_Hosting_Ollama_Web>("webfrontend")
