4c4c6486080524468b66cb0d91d70b6dc207c23b
Aaron Powell <me@aaron-powell.com>
2024-09-27 05:00:59 +0000

    Combining tests to see if this makes them more consistent

--- tests/Aspire.CommunityToolkit.Hosting.Java.Tests/JavaHostingComponentTests.cs
+++ tests/Aspire.CommunityToolkit.Hosting.Java.Tests/JavaHostingComponentTests.cs
@@ -7,25 +7,12 @@ namespace Aspire.CommunityToolkit.Hosting.Java.Tests;
 #pragma warning disable CTASPIRE001
 public class JavaHostingComponentTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Java_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Java_AppHost>>
 {
-    [ConditionalFact]
+    [ConditionalTheory]
     [OSSkipCondition(OperatingSystems.Windows)]
-    public async Task ContainerAppResourceWillRespondWithOk()
+    [InlineData("containerapp")]
+    [InlineData("executableapp")]
+    public async Task AppResourceWillRespondWithOk(string resourceName)
     {
-        var resourceName = "containerapp";
-        var httpClient = fixture.CreateHttpClient(resourceName);
-
-        await fixture.App.WaitForTextAsync("Started SpringMavenApplication", resourceName).WaitAsync(TimeSpan.FromMinutes(5));
-
-        var response = await httpClient.GetAsync("/");
-
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-    }
-
-    [ConditionalFact]
-    [OSSkipCondition(OperatingSystems.Windows)]
-    public async Task ExecutableAppResourceWillRespondWithOk()
-    {
-        var resourceName = "executableapp";
         var httpClient = fixture.CreateHttpClient(resourceName);
 
         await fixture.App.WaitForTextAsync("Started SpringMavenApplication", resourceName).WaitAsync(TimeSpan.FromMinutes(5));
