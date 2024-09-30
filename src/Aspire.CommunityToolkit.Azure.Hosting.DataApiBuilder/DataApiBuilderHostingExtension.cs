0f7426c0d5e7504861b297148d4155fb977ed45f
Tommaso Stocchi <tstocchi@microsoft.com>
2024-09-30 16:21:12 +0200

    fixes the data api builder container creation

--- src/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder/DataApiBuilderHostingExtension.cs
+++ src/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder/DataApiBuilderHostingExtension.cs
@@ -37,56 +37,26 @@ public static class DataApiBuilderHostingExtension
           .WithHttpEndpoint(port: options.Port, targetPort: options.TargetPort, name: DataApiBuilderContainerResource.HttpEndpointName)
           .WithDataApiBuilderDefaults(options);
 
-        if(string.IsNullOrWhiteSpace(options.ConfigFilePath) == false)
+        if(string.IsNullOrWhiteSpace(options.ConfigFilePath))
         {
             throw new ArgumentException("The DataApiBuilder configuration file must be specified.", nameof(options));
         }
-        rb.WithVolume(PathNormalizer.NormalizePathForCurrentPlatform(options.ConfigFilePath), "/App/dab-config.json");
+        rb.WithBindMount(PathNormalizer.NormalizePathForCurrentPlatform(options.ConfigFilePath), "/App/dab-config.json", true);
 
         return rb;
     }
 
     /// <summary>
-    /// Adds a Spring application to the application model. Executes the containerized Spring app.
+    /// Adds a DataAPIBuilder application to the application model. Executes the containerized DataAPIBuilder app.
     /// </summary>
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/> to add the resource to.</param>
     /// <param name="name">The name of the resource.</param>
     /// <param name="options">The <see cref="DataApiBuilderContainerResourceOptions"/> to configure the DataApiBuilder api.</param>"
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
-    public static IResourceBuilder<DataApiBuilderContainerResource> AddSpringApp(this IDistributedApplicationBuilder builder, string name, DataApiBuilderContainerResourceOptions options)
+    public static IResourceBuilder<DataApiBuilderContainerResource> AddDataAPIBuilderApp(this IDistributedApplicationBuilder builder, string name, DataApiBuilderContainerResourceOptions options = null)
     {
+        options ??= new DataApiBuilderContainerResourceOptions();
         return builder.AddDataApiBuilder(name, options);
-    }
-
-    /// <summary>
-    /// Adds a DataApiBuilder api to the application model. Executes the executable DataApiBuilder api.
-    /// </summary>
-    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/> to add the resource to.</param>
-    /// <param name="name">The name of the resource.</param>
-    /// <param name="workingDirectory">The working directory to use for the command. If null, the working directory of the current process is used.</param>
-    /// <param name="options">The <see cref="DataApiBuilderExecutableResourceOptions"/> to configure the DataApiBuilder api.</param>"
-    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
-    public static IResourceBuilder<DataApiBuilderExecutableResource> AddDataApiBuilder(this IDistributedApplicationBuilder builder, string name, string workingDirectory, DataApiBuilderExecutableResourceOptions options)
-    {
-        workingDirectory = PathNormalizer.NormalizePathForCurrentPlatform(Path.Combine(builder.AppHostDirectory, workingDirectory));
-        var resource = new DataApiBuilderExecutableResource(name, "dab", workingDirectory);
-
-        return builder.AddResource(resource)
-                      .WithDataApiBuilderDefaults(options)
-                      .WithHttpEndpoint(port: options.Port, name: DataApiBuilderContainerResource.HttpEndpointName, isProxied: false);
-    }
-
-    /// <summary>
-    /// Adds a Spring application to the application model. Executes the executable Spring app.
-    /// </summary>
-    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/> to add the resource to.</param>
-    /// <param name="name">The name of the resource.</param>
-    /// <param name="workingDirectory">The working directory to use for the command. If null, the working directory of the current process is used.</param>
-    /// <param name="options">The <see cref="DataApiBuilderExecutableResourceOptions"/> to configure the DataApiBuilder api.</param>"
-    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
-    public static IResourceBuilder<DataApiBuilderExecutableResource> AddSpringApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, DataApiBuilderExecutableResourceOptions options)
-    {
-        return builder.AddDataApiBuilder(name, workingDirectory, options);
     }
 
     private static IResourceBuilder<DataApiBuilderContainerResource> WithDataApiBuilderDefaults(
@@ -94,12 +64,5 @@ public static class DataApiBuilderHostingExtension
         DataApiBuilderContainerResourceOptions options) =>
         builder.WithOtlpExporter()
                .WithEnvironment("DATAAPIBUILDER_TOOL_OPTIONS", $"-dabagent:{"options.OtelAgentPath"?.TrimEnd('/')}/opentelemetry-dabagent.jar")
-               ;
-
-    private static IResourceBuilder<DataApiBuilderExecutableResource> WithDataApiBuilderDefaults(
-        this IResourceBuilder<DataApiBuilderExecutableResource> builder,
-        DataApiBuilderExecutableResourceOptions options) =>
-        builder.WithOtlpExporter()
-               .WithEnvironment("SERVER_PORT", options.Port.ToString(CultureInfo.InvariantCulture))
                ;
 }
