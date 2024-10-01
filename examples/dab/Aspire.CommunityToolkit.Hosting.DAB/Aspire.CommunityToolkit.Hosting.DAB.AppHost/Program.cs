695fb45598818a1943af3ea114c91c182e8aa468
Frank <fboucher@outlook.com>
2024-10-01 15:15:35 -0400

    adding comments

--- examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Aspire.CommunityToolkit.Hosting.DAB.AppHost/Program.cs
+++ examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Aspire.CommunityToolkit.Hosting.DAB.AppHost/Program.cs
@@ -2,6 +2,7 @@ using Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add a SQL Server container
 var sqlPassword = builder.AddParameter("sql-password");
 var sqlServer = builder
     .AddSqlServer("sql", sqlPassword)
@@ -9,13 +10,15 @@ var sqlServer = builder
     .WithHealthCheck();
 var sqlDatabase = sqlServer.AddDatabase("trek");
 
+// Populate the database with the schema and data
 sqlServer
     .WithBindMount("./sql-server", target: "/usr/config")
     .WithBindMount("../Database/sql", target: "/docker-entrypoint-initdb.d")
     .WithEntrypoint("/usr/config/entrypoint.sh");
 
+// Add Data API Builder using dab-config.json 
 var dab = builder.AddDataAPIBuilder("dab")
-    .WithReference(sqlDatabase)
+    .WithReference(sqlServer)
     .WaitFor(sqlDatabase);
     
 builder.Build().Run();
