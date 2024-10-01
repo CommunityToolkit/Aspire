4ae24d3bfa50fa748e529cee115c0a81972adffb
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-01 12:40:53 +0200

    initiliaze example with db integration

---
+++ examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Database/dbo/Tables/Users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Users] (
+    [Id]       INT           NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [LastName] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
