05b75fbd61aae5acf3378acf69a438bb1dc0b64a
Aaron Powell <me@aaron-powell.com>
2024-10-01 06:52:03 +0000

    Borrowing more code from the aspire repo to try and make tests easier

--- tests/Aspire.CommunityToolkit.Testing/AspireIntegrationTest.cs
+++ tests/Aspire.CommunityToolkit.Testing/AspireIntegrationTest.cs
@@ -1,4 +1,5 @@
-﻿using Aspire.Hosting;
+﻿using Aspire.Components.Common.Tests;
+using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -32,7 +33,11 @@ public class AspireIntegrationTestFixture<TEntryPoint>() : DistributedApplicatio
         base.OnBuilderCreated(applicationBuilder);
     }
 
-    public async Task InitializeAsync() => await StartAsync();
+    public async Task InitializeAsync()
+    {
+        if (RequiresDockerAttribute.IsSupported)
+            await StartAsync();
+    }
 
     async Task IAsyncLifetime.DisposeAsync()
     {
@@ -42,7 +47,7 @@ public class AspireIntegrationTestFixture<TEntryPoint>() : DistributedApplicatio
         }
         catch (Exception)
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("CI")))
+            if (RequiresDockerAttribute.IsSupported)
             {
                 // GitHub Actions Windows runners don't support Linux Docker containers, which can result in a bunch of false errors, even if we try to skip the test run, so we only really want to throw
                 // if we're on a non-Windows runner or if we're on a Windows runner but not in a CI environment
