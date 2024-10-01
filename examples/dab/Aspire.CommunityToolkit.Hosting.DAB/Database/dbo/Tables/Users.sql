7591f68cb058b0d3fc3a0d9eee092a88704af27d
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-01 14:22:32 +0200

    adds database_create sql script and adapts dab-config

--- examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Database/dbo/Tables/Users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[Users] (
-    [Id]       INT           NOT NULL,
-    [FirstName] NVARCHAR (50) NOT NULL,
-    [LastName] NVARCHAR (50) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
