ac6a45ec4f624301445104d985acd8591ade66b9
Frank <fboucher@outlook.com>
2024-10-01 15:05:59 -0400

    fix start

--- examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Aspire.CommunityToolkit.Hosting.DAB.AppHost/Program.cs
+++ examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Aspire.CommunityToolkit.Hosting.DAB.AppHost/Program.cs
@@ -4,20 +4,18 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var sqlPassword = builder.AddParameter("sql-password");
 var sqlServer = builder
-    .AddSqlServer("sql", sqlPassword, port: 1433)
-    .WithDataVolume("MyDataVolume")
+    .AddSqlServer("sql", sqlPassword)
+    //.WithDataVolume("MyDataVolume") // Uncomment to persist the data into volume
     .WithHealthCheck();
 var sqlDatabase = sqlServer.AddDatabase("trek");
 
-var sqlShell = "./sql-server";
-var sqlScript = "../Database/sql";
 sqlServer
-    .WithBindMount(sqlShell, target: "/usr/config")
-    .WithBindMount(sqlScript, target: "/docker-entrypoint-initdb.d")
+    .WithBindMount("./sql-server", target: "/usr/config")
+    .WithBindMount("../Database/sql", target: "/docker-entrypoint-initdb.d")
     .WithEntrypoint("/usr/config/entrypoint.sh");
 
 var dab = builder.AddDataAPIBuilder("dab")
     .WithReference(sqlDatabase)
-    .WaitFor(sqlServer);
-
+    .WaitFor(sqlDatabase);
+    
 builder.Build().Run();
