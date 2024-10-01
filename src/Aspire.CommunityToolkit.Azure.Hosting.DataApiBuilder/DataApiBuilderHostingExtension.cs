8abef78dea2dd1d5752a96b3c369dcab486aebcb
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-01 11:22:35 +0200

    improves dab creation by passing in parameter instead of DataAPIBuilderContainerResourceOptions

--- src/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder/DataApiBuilderHostingExtension.cs
+++ src/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder/DataApiBuilderHostingExtension.cs
@@ -61,10 +61,22 @@ public static class DataApiBuilderHostingExtension
     /// <param name="name">The name of the resource.</param>
     /// <param name="configFilePath">The path to the config file for Data API Builder.</param>"
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
-    public static IResourceBuilder<DataApiBuilderContainerResource> AddDataAPIBuilder(this IDistributedApplicationBuilder builder, string name, string configFilePath)
+    public static IResourceBuilder<DataApiBuilderContainerResource> AddDataAPIBuilder(this IDistributedApplicationBuilder builder, 
+        string name, 
+        string configFilePath = "./dab-config.json", 
+        string containerRegistry = "mcr.microsoft.com", 
+        string containerImageName = "azure-databases/data-api-builder",
+        string containerImageTag = "latest",
+        int port = 5000,
+        int targetPort = 5000)
     {
         var options = new DataApiBuilderContainerResourceOptions
         {
+            ContainerRegistry = containerRegistry,
+            ContainerImageName = containerImageName,
+            ContainerImageTag = containerImageTag,
+            Port = port,
+            TargetPort = targetPort,
             ConfigFilePath = configFilePath
         };
         return builder.AddDataAPIBuilder(name, options);
