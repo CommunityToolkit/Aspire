14805148e818959813bfb541a4f9a6b692a4fb9a
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-01 12:40:53 +0200

    initiliaze example with db integration

--- examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Aspire.CommunityToolkit.Hosting.DAB.AppHost/Program.cs
+++ examples/dab/Aspire.CommunityToolkit.Hosting.DAB/Aspire.CommunityToolkit.Hosting.DAB.AppHost/Program.cs
@@ -2,9 +2,22 @@ using Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var sql = builder.AddConnectionString("sqldb");
+var sqlPassword = builder.AddParameter("sql-password");
+var sqlServer = builder
+    .AddSqlServer("sql", sqlPassword, port: 1234)
+    .WithDataVolume("MyDataVolume")
+    .WithHealthCheck();
+var sqlDatabase = sqlServer.AddDatabase("Database");
+
+var sqlShell = "./sql-server";
+var sqlScript = "../Database";
+sqlServer
+    .WithBindMount(sqlShell, target: "/usr/config")
+    .WithBindMount(sqlScript, target: "/docker-entrypoint-initdb.d")
+    .WithEntrypoint("/usr/config/entrypoint.sh");
 
 var dab = builder.AddDataAPIBuilder("dab")
-    .WithReference(sql);
+    .WithReference(sqlDatabase)
+    .WaitFor(sqlServer);
 
 builder.Build().Run();
