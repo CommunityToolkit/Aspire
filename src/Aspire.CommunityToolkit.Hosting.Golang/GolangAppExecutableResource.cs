2307d15e27a2cbd0fd3e02cf6caf30e417014998
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-02 09:54:46 +0200

    refactors GolangAppExecutableResource

--- src/Aspire.CommunityToolkit.Hosting.Golang/GolangAppExecutableResource.cs
+++ src/Aspire.CommunityToolkit.Hosting.Golang/GolangAppExecutableResource.cs
@@ -1,16 +1,13 @@
-﻿using Aspire.Hosting;
-using Aspire.Hosting.ApplicationModel;
-
-namespace Aspire.CommunityToolkit.Hosting.Golang;
+﻿namespace Aspire.Hosting.ApplicationModel;
 
 /// <summary>
 /// A resource that represents a Golang application.
 /// </summary>
 /// <param name="name">The name of the resource.</param>
 /// <param name="command">The command to execute.</param>
-/// <param name="workingDirectory">The working directory to use for the command. If null, the working directory of the current process is used.</param>
-public class GolangAppExecutableResource(string name, string command, string workingDirectory)
-    : ExecutableResource(name, command, workingDirectory), IResourceWithServiceDiscovery
+/// <param name="workingDirectory">The working directory to use for the command.</param>
+public class GolangAppExecutableResource(string name, string workingDirectory)
+    : ExecutableResource(name, "go", workingDirectory), IResourceWithServiceDiscovery
 {
     internal const string HttpEndpointName = "http";
 }
--- src/Aspire.CommunityToolkit.Hosting.Golang/GolangAppHostingExtension.cs
+++ src/Aspire.CommunityToolkit.Hosting.Golang/GolangAppHostingExtension.cs
@@ -1,8 +1,7 @@
 ﻿using Aspire.CommunityToolkit.Hosting.Golang.Utils;
-using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
 
-namespace Aspire.CommunityToolkit.Hosting.Golang;
+namespace Aspire.Hosting;
 
 /// <summary>
 /// Provides extension methods for adding Golang applications to an <see cref="IDistributedApplicationBuilder"/>.
@@ -15,30 +14,27 @@ public static class GolangAppHostingExtension
     /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/> to add the resource to.</param>
     /// <param name="name">The name of the resource.</param>
     /// <param name="workingDirectory">The working directory to use for the command. If null, the working directory of the current process is used.</param>
-    /// <param name="options">The <see cref="GolangAppExecutableResourceOptions"/> to configure the Golang application.</param>"
+    /// <param name="port">This is the port that will be given to other resource to communicate with this resource.</param>"
+    /// <param name="args">The optinal arguments to be passed to the executable when it is started.</param>
     /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
-    public static IResourceBuilder<GolangAppExecutableResource> AddGolangApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, GolangAppExecutableResourceOptions options)
+    public static IResourceBuilder<GolangAppExecutableResource> AddGolangApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, int port = 8080, string[]? args = null) 
     {
         ArgumentNullException.ThrowIfNull(builder, nameof(builder));
-        ArgumentNullException.ThrowIfNull(options, nameof(options));
         ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
         ArgumentException.ThrowIfNullOrWhiteSpace(workingDirectory, nameof(workingDirectory));
 
-        string[] allArgs = options.Args is { Length: > 0 }
-            ? ["run", ".", .. options.Args] 
+        string[] allArgs = args is { Length: > 0 }
+            ? ["run", ".", .. args] 
             : ["run", ".",];
 
         workingDirectory = PathNormalizer.NormalizePathForCurrentPlatform(Path.Combine(builder.AppHostDirectory, workingDirectory));
-        var resource = new GolangAppExecutableResource(name, "go", workingDirectory);
+        var resource = new GolangAppExecutableResource(name, workingDirectory);
 
         return builder.AddResource(resource)
                       .WithGolangDefaults()
-                      .WithHttpEndpoint(port: options.Port, name: GolangAppExecutableResource.HttpEndpointName, isProxied: false)
+                      .WithHttpEndpoint(port: port, name: GolangAppExecutableResource.HttpEndpointName, isProxied: false)
                       .WithArgs(allArgs);
     }
-
-    public static IResourceBuilder<GolangAppExecutableResource> AddGolangApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, int port = 8080, string[]? args = null) =>
-        builder.AddGolangApp(name, workingDirectory, new GolangAppExecutableResourceOptions { Port = port, Args = args });
 
     private static IResourceBuilder<GolangAppExecutableResource> WithGolangDefaults(
         this IResourceBuilder<GolangAppExecutableResource> builder) =>
