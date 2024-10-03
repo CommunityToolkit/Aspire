a0417fb998c98a4a30e94163d61f215a35662f4a
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-03 11:54:39 +0200

    improves test for AppHost golang

--- tests/Aspire.CommunityToolkit.Hosting.Golang.Tests/AppHostTests.cs
+++ tests/Aspire.CommunityToolkit.Hosting.Golang.Tests/AppHostTests.cs
@@ -4,15 +4,16 @@ using Microsoft.TestUtilities;
 
 namespace Aspire.CommunityToolkit.Hosting.Golang.Tests;
 
+#pragma warning disable CTASPIRE001
 public class AppHostTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Golang_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_Golang_AppHost>>
 {
-    [ConditionalFact]
-    public async Task ResourceStartsAndRespondsOk()
+    [Theory]
+    [InlineData("golang")]
+    public async Task ResourceStartsAndRespondsOk(string appName)
     {
-        await fixture.ResourceNotificationService.WaitForResourceAsync("golang", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
-        var httpClient = fixture.CreateHttpClient("golang");
+        var httpClient = fixture.CreateHttpClient(appName);
 
-        await Task.Delay(30000);
+        await fixture.App.WaitForTextAsync("Listening and serving HTTP on :8080", appName).WaitAsync(TimeSpan.FromMinutes(1));
 
         var response = await httpClient.GetAsync("/ping");
 
