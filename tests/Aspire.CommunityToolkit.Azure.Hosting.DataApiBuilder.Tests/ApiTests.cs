0377d23392bfd237d01810ee5edd59af8bebc3fe
Frank <fboucher@outlook.com>
2024-10-03 17:01:44 -0400

    add a test. but it's failing

---
+++ tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/ApiTests.cs
@@ -0,0 +1,34 @@
+﻿using Aspire.CommunityToolkit.Hosting.DataApiBuilder.BlazorApp;
+using Aspire.CommunityToolkit.Testing;
+using FluentAssertions;
+using Microsoft.TestUtilities;
+using System.Net.Http.Json;
+
+namespace Aspire.CommunityToolkit.Hosting.DataApiBuilder.Tests;
+
+public class ApiTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_DataApiBuilder_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_DataApiBuilder_AppHost>>
+{
+
+    [ConditionalFact]
+    
+    public async Task CanGetSeries()
+    {
+
+        await fixture.ResourceNotificationService.WaitForResourceAsync("dab", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        
+        var httpClient = fixture.CreateHttpClient("dab", "http");
+
+        //Error: Unable to find config file: dab-config.json does not exist.
+
+        var response = await httpClient.GetAsync("/api/series");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var series = await response.Content.ReadFromJsonAsync<SeriesList>();
+        series.Should().NotBeNull();
+        series.value.Should().NotBeNull();
+        series.value.Should().HaveCount(5);
+    }
+
+
+}
+
