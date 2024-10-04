c0702155c1c14d31b05e060ef075362533a916b9
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-04 11:06:22 +0200

    Update tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
    Co-authored-by: Aaron Powell <me@aaron-powell.com>

--- tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
+++ tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
@@ -7,6 +7,7 @@ namespace Aspire.CommunityToolkit.Hosting.DataApiBuilder.Tests;
 public class AppHostTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_DataApiBuilder_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_DataApiBuilder_AppHost>>
 {
     [ConditionalFact]
+    [OSSkipCondition(OperatingSystems.Windows)]
     public async Task ResourceStartsAndRespondsOk()
     {
         await fixture.ResourceNotificationService.WaitForResourceAsync("dab", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
