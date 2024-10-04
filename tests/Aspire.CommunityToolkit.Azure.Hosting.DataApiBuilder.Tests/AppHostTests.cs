5f2ffeb8db1ffe98a52982229369ec8d91224764
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-04 11:05:14 +0200

    changes parameters for dab container creation

--- tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
+++ tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
@@ -9,7 +9,7 @@ public class AppHostTests(AspireIntegrationTestFixture<Projects.Aspire_Community
     [ConditionalFact]
     public async Task ResourceStartsAndRespondsOk()
     {
-        await fixture.ResourceNotificationService.WaitForResourceAsync("dab", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
+        await fixture.ResourceNotificationService.WaitForResourceAsync("dab", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(1));
         var httpClient = fixture.CreateHttpClient("dab", "http");
 
         var response = await httpClient.GetAsync("/");
