7c3ce987c8344933888713d5dd4db8f1a426722b
alireza.baloochi1380@gmail.com <alireza.baloochi1380@gmail.com>
2024-10-04 21:34:35 +0330

    Use RequiresDocker in tests

--- tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
+++ tests/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder.Tests/AppHostTests.cs
@@ -1,13 +1,12 @@
 using Aspire.CommunityToolkit.Testing;
+using Aspire.Components.Common.Tests;
 using FluentAssertions;
-using Microsoft.TestUtilities;
 
 namespace Aspire.CommunityToolkit.Hosting.DataApiBuilder.Tests;
 
 public class AppHostTests(AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_DataApiBuilder_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Aspire_CommunityToolkit_Hosting_DataApiBuilder_AppHost>>
 {
-    [ConditionalFact]
-    [OSSkipCondition(OperatingSystems.Windows)]
+    [RequiresDocker]
     public async Task ResourceStartsAndRespondsOk()
     {
         await fixture.ResourceNotificationService.WaitForResourceAsync("dab", KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(1));
