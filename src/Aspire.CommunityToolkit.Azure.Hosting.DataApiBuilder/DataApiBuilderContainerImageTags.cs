dc241ba75d21b4a62fc9f4861b2c785f24e90524
Tommaso Stocchi <tstocchi@microsoft.com>
2024-10-06 23:52:45 +0200

    uses pinned version for dab container

--- src/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder/DataApiBuilderContainerImageTags.cs
+++ src/Aspire.CommunityToolkit.Azure.Hosting.DataApiBuilder/DataApiBuilderContainerImageTags.cs
@@ -4,5 +4,5 @@ internal static class DataApiBuilderContainerImageTags
 {
     public const string Registry = "mcr.microsoft.com";
     public const string Image = "azure-databases/data-api-builder";
-    public const string Tag = "latest";
+    public const string Tag = "1.2.11";
 }
