2ee554c5c2db83eb29d258dc7ca4e141fcf3da1d
Aaron Powell <me@aaron-powell.com>
2024-10-22 05:04:35 +0000

    Matching the correct naming structure

--- examples/meilisearch/Aspire.CommunityToolkit.Hosting.Meilisearch.ApiService/Movie.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Aspire.CommunityToolkit.Hosting.Meilisearch.ApiService;
-public class Movie
-{
-    public string? Id { get; set; }
-    public string? Title { get; set; }
-    public IEnumerable<string>? Genres { get; set; }
-}
---
+++ src/CommunityToolkit.Aspire.Hosting.Meilisearch/MeilisearchContainerImageTags.cs
@@ -0,0 +1,12 @@
+namespace CommunityToolkit.Aspire.Hosting.Meilisearch;
+
+internal static class MeilisearchContainerImageTags
+{
+    /// <summary>docker.io</summary>
+    public const string Registry = "docker.io";
+    /// <summary>getmeili/meilisearch</summary>
+    public const string Image = "getmeili/meilisearch";
+    /// <summary>v1.10</summary>
+    public const string Tag = "v1.10";
+}
+
