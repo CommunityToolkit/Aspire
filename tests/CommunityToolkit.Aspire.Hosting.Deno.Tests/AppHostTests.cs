e361821eda7328b8608a5c255a136cdf26bc0a70
Brian Guerrero <13bguerrero@gmail.com>
2024-10-24 06:55:33 -0500

    Update AppHostTests.cs
    Co-authored-by: Aaron Powell <me@aaron-powell.com>

--- tests/CommunityToolkit.Aspire.Hosting.Deno.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Deno.Tests/AppHostTests.cs
@@ -6,10 +6,10 @@ namespace CommunityToolkit.Aspire.Hosting.Deno.Tests;
 #pragma warning disable CTASPIRE001
 public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Deno_Apphost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Deno_Apphost>>
 {
-    [Theory]
-    [InlineData("vite-demo")]
-    public async Task ResourceStartsAndRespondsOk(string appName)
+    [Fact]
+    public async Task ResourceStartsAndRespondsOk()
     {
+        var appName = "vite-demo";
         var httpClient = fixture.CreateHttpClient(appName);
 
         await fixture.App.WaitForTextAsync("VITE", appName).WaitAsync(TimeSpan.FromSeconds(30));
