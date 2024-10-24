720b6a49e1c11c775928e68aa08aff24c4363052
Brian Guerrero <13bguerrero@gmail.com>
2024-10-24 00:13:01 -0500

    Cleaned up Deno config and add README for Aspire Hosting Deno package.

--- examples/deno/CommunityToolkit.Aspire.Hosting.Deno.Apphost/Program.cs
+++ examples/deno/CommunityToolkit.Aspire.Hosting.Deno.Apphost/Program.cs
@@ -2,14 +2,12 @@ using CommunityToolkit.Aspire.Hosting.Deno;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder
-    .AddDenoTask("vite-demo", taskName: "dev")
+builder.AddDenoTask("vite-demo", taskName: "dev")
     .WithDenoPackageInstallation()
     .WithHttpEndpoint(env: "PORT")
     .WithEndpoint();
 
 builder.AddDenoApp("oak-demo", "main.ts", permissionFlags: ["-E", "--allow-net"])
-    .WithDenoPackageInstallation()
     .WithHttpEndpoint(env: "PORT")
     .WithEndpoint();
 
