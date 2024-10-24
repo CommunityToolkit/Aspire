7021e6208264d614303088f3005ca8be2c2e40e5
Aaron Powell <me@aaron-powell.com>
2024-10-24 04:37:13 +0000

    Adding all the missing docs

--- src/CommunityToolkit.Aspire.Hosting.Golang/GolangAppExecutableResource.cs
+++ src/CommunityToolkit.Aspire.Hosting.Golang/GolangAppExecutableResource.cs
@@ -4,7 +4,6 @@
 /// A resource that represents a Golang application.
 /// </summary>
 /// <param name="name">The name of the resource.</param>
-/// <param name="command">The command to execute.</param>
 /// <param name="workingDirectory">The working directory to use for the command.</param>
 public class GolangAppExecutableResource(string name, string workingDirectory)
     : ExecutableResource(name, "go", workingDirectory), IResourceWithServiceDiscovery
