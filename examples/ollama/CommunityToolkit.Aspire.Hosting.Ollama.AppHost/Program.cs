f37d28458a8ce9a5304a7cac30d15858700ed485
Aaron Powell <me@aaron-powell.com>
2024-11-06 22:45:01 +0000

    Removing the code from testing

--- examples/ollama/CommunityToolkit.Aspire.Hosting.Ollama.AppHost/Program.cs
+++ examples/ollama/CommunityToolkit.Aspire.Hosting.Ollama.AppHost/Program.cs
@@ -1,7 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var ollama = builder.AddOllama("ollama")
-    .WithDataVolume()
     .WithOpenWebUI();
 
 var phi3 = ollama.AddModel("phi3", "phi3");
