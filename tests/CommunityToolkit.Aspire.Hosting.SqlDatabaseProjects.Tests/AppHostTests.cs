eec2b39393155b00f8c43a5353701884b12b3c9d
Jonathan Mezach <jonathan.mezach@rr-wfm.com>
2024-11-07 08:00:10 +0100

    Add assertion for created table
    Signed-off-by: Jonathan Mezach <jonathan.mezach@rr-wfm.com>

--- tests/CommunityToolkit.Aspire.Hosting.SqlDatabaseProjects.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.SqlDatabaseProjects.Tests/AppHostTests.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Aspire.Testing;
 using Aspire.Components.Common.Tests;
+using Microsoft.Data.SqlClient;
 
 namespace CommunityToolkit.Aspire.Hosting.SqlDatabaseProjects.Tests;
 
@@ -11,5 +12,21 @@ public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit
     {
         string resourceName = "sdk-project";
         await fixture.ResourceNotificationService.WaitForResourceAsync(resourceName, KnownResourceStates.Finished).WaitAsync(TimeSpan.FromMinutes(5));
+
+        string? connectionString = await fixture.GetConnectionString("TargetDatabase");
+        Assert.NotNull(connectionString);
+
+        using var connection = new SqlConnection(connectionString);
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText =
+            "SELECT COUNT(1) " +
+            "FROM   INFORMATION_SCHEMA.TABLES " +
+            "WHERE  TABLE_SCHEMA = 'dbo' " +
+            "AND    TABLE_NAME = 'SdkProject'";
+        
+        var result = await command.ExecuteScalarAsync();
+        Assert.Equal(1, result);
     }
 }
