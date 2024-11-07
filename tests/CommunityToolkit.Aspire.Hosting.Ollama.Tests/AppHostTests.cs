50779c7382ef57487d4aa6f823130c0636f8c040
Aaron Powell <me@aaron-powell.com>
2024-11-07 05:40:33 +0000

    Putting some timeouts around and adding a new test

--- tests/CommunityToolkit.Aspire.Hosting.Ollama.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Ollama.Tests/AppHostTests.cs
@@ -1,6 +1,7 @@
 using CommunityToolkit.Aspire.Testing;
 using Aspire.Components.Common.Tests;
 using FluentAssertions;
+using OllamaSharp;
 
 namespace CommunityToolkit.Aspire.Hosting.Ollama.Tests;
 
@@ -18,5 +19,24 @@ public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit
         var response = await httpClient.GetAsync("/");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task OllamaListsAvailableModels()
+    {
+        var distributedAppModel = fixture.App.Services.GetRequiredService<DistributedApplicationModel>();
+        var modelResources = distributedAppModel.Resources.OfType<OllamaModelResource>();
+        var rns = fixture.ResourceNotificationService;
+
+        await Task.WhenAll([
+                rns.WaitForResourceHealthyAsync("ollama"),
+                .. modelResources.Select(m => rns.WaitForResourceHealthyAsync(m.Name))
+            ]).WaitAsync(TimeSpan.FromMinutes(5));
+        var httpClient = fixture.CreateHttpClient("ollama");
+
+        var models = await new OllamaApiClient(httpClient).ListLocalModelsAsync();
+
+        models.Should().NotBeEmpty();
+        models.Should().HaveCount(modelResources.Count());
     }
 }
