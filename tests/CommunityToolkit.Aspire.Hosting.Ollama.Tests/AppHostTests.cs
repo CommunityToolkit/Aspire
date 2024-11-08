4f55ecbdd931c0173222e412a08040b77ef36c6f
Aaron Powell <me@aaron-powell.com>
2024-11-08 03:23:22 +0000

    Adding another test

--- tests/CommunityToolkit.Aspire.Hosting.Ollama.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Ollama.Tests/AppHostTests.cs
@@ -8,9 +8,9 @@ namespace CommunityToolkit.Aspire.Hosting.Ollama.Tests;
 [RequiresDocker]
 public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Ollama_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Ollama_AppHost>>
 {
-    [Theory(Skip = "This test is flaky")]
+    [Theory]
     [InlineData("ollama")]
-    [InlineData("ollama-openwebui")]
+    // [InlineData("ollama-openwebui")]
     public async Task ResourceStartsAndRespondsOk(string resourceName)
     {
         await fixture.ResourceNotificationService.WaitForResourceAsync(resourceName, KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
