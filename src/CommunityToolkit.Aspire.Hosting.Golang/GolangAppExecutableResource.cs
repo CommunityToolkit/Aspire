28c133d5e7e3f43f261d0826da2754d1ceb1ca00
Tommaso Stocchi <tstocchi@microsoft.com>
2024-11-10 00:05:04 +0100

    golang port config demanded to AppHost

--- src/CommunityToolkit.Aspire.Hosting.Golang/GolangAppExecutableResource.cs
+++ src/CommunityToolkit.Aspire.Hosting.Golang/GolangAppExecutableResource.cs
@@ -6,7 +6,4 @@
 /// <param name="name">The name of the resource.</param>
 /// <param name="workingDirectory">The working directory to use for the command.</param>
 public class GolangAppExecutableResource(string name, string workingDirectory)
-    : ExecutableResource(name, "go", workingDirectory), IResourceWithServiceDiscovery
-{
-    internal const string HttpEndpointName = "http";
-}
+    : ExecutableResource(name, "go", workingDirectory), IResourceWithServiceDiscovery;
