a8ab95d3407c009b1df90f9e1fe829c45c27bbea
Tommaso Stocchi <tstocchi@microsoft.com>
2024-11-11 13:57:12 +0100

    adds tests

---
+++ tests/CommunityToolkit.Aspire.Hosting.Uvicorn.Tests/ResourceCreationTests.cs
@@ -0,0 +1,24 @@
+using Aspire.Hosting;
+
+namespace CommunityToolkit.Aspire.Hosting.Uvicorn.Extensions.Tests;
+
+public class ResourceCreationTests
+{
+    [Fact]
+    public void DefaultUvicornApp()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+
+        builder.AddUvicornApp("uvicornapp", "../../examples/uvicorn/uvicornapp-api", "main:app");
+
+        using var app = builder.Build();
+
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+
+        var resource = appModel.Resources.OfType<UvicornAppResource>().SingleOrDefault();
+
+        Assert.NotNull(resource);
+
+        Assert.Equal("uvicorn", resource.Command);
+    }
+}
