d7495880ad2963417c01c9e484fca6759a43cf96
Aaron Powell <me@aaron-powell.com>
2024-11-12 01:24:42 +0000

    Using ilogger instead

--- tests/CommunityToolkit.Aspire.Hosting.Rust.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Rust.Tests/AppHostTests.cs
@@ -1,12 +1,13 @@
 using CommunityToolkit.Aspire.Testing;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using Xunit.Abstractions;
 
 namespace CommunityToolkit.Aspire.Hosting.Rust.Tests;
 
 public class AppHostTests(
-    AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Rust_AppHost> fixture, ITestOutputHelper outputHelper) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Rust_AppHost>>
+    AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Rust_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Rust_AppHost>>
 {
     [Fact]
     public async Task ResourceStartsAndRespondsOk()
@@ -14,18 +15,20 @@ public class AppHostTests(
         var appName = "rust-app";
         var httpClient = fixture.CreateHttpClient(appName);
 
-        outputHelper.WriteLine($"[Custom]: Waiting for resource {appName} to be healthy...");
+        var logger = fixture.App.Services.GetRequiredService<ILogger<AppHostTests>>();
+
+        logger.LogInformation($"[Custom]: Waiting for resource {appName} to be healthy...");
 
         var rns = fixture.App.Services.GetRequiredService<ResourceNotificationService>();
         var re = await rns.WaitForResourceHealthyAsync(appName).WaitAsync(TimeSpan.FromMinutes(10));
 
-        outputHelper.WriteLine($"[Custom]: Resource {appName} is healthy!");
-        outputHelper.WriteLine($"[Custom]: Resource event: {JsonSerializer.Serialize(re, new JsonSerializerOptions { WriteIndented = true })}");
-        outputHelper.WriteLine("[Custom]: Pinging the resource...");
+        logger.LogInformation($"[Custom]: Resource {appName} is healthy!");
+        logger.LogInformation($"[Custom]: Resource event: {JsonSerializer.Serialize(re, new JsonSerializerOptions { WriteIndented = true })}");
+        logger.LogInformation("[Custom]: Pinging the resource...");
 
         var response = await httpClient.GetAsync("/ping");
 
-        outputHelper.WriteLine($"[Custom]: Response: {response.StatusCode}");
+        logger.LogInformation($"[Custom]: Response: {response.StatusCode}");
 
         response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
