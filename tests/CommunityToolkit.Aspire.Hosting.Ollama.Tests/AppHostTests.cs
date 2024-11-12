2c7a682ed1146a0dde65279ace3af9992aeb838f
Aaron Powell <me@aaron-powell.com>
2024-11-12 05:16:45 +0000

    Using a new app host in the ollama tests and removing OpenWebUI
    It looks like we keep disk space issues in GitHub Action runners (docs say they have 14gb of storage), so it's time to do some rethinking of Ollama in particular.
    
    Ollama itself is a ~3gb image, then we were pulling two models, each 2gb or greater, then Open WebUI, which is ~4gb. Combine that with SQL, DAB, Meilisearch, and so on, and we're getting quite large with on-disk usage.
    
    So with Ollama we're now using a custom app host for testing which slims down what's installed, only Ollama and tinyllama as a model (all up that should be around 4gb, not the more than 10gb we used to use)

--- tests/CommunityToolkit.Aspire.Hosting.Ollama.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Ollama.Tests/AppHostTests.cs
@@ -6,13 +6,12 @@ using OllamaSharp;
 namespace CommunityToolkit.Aspire.Hosting.Ollama.Tests;
 
 [RequiresDocker]
-public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Ollama_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Ollama_AppHost>>
+public class AppHostTests(AspireIntegrationTestFixture<Projects.Ollama_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.Ollama_AppHost>>
 {
-    [Theory]
-    [InlineData("ollama")]
-    [InlineData("ollama-openwebui")]
-    public async Task ResourceStartsAndRespondsOk(string resourceName)
+    [Fact]
+    public async Task ResourceStartsAndRespondsOk()
     {
+        var resourceName = "ollama";
         await fixture.ResourceNotificationService.WaitForResourceAsync(resourceName, KnownResourceStates.Running).WaitAsync(TimeSpan.FromMinutes(5));
         var httpClient = fixture.CreateHttpClient(resourceName);
 
---
+++ tests/custom-app-hosts/Ollama.AppHost/Program.cs
@@ -0,0 +1,7 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var ollama = builder.AddOllama("ollama");
+
+var tinyllama = ollama.AddModel("tinyllama");
+
+builder.Build().Run();
