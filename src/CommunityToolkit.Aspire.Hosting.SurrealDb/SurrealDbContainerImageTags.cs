b8d5164b2ebd058b48e175d381689868bc91db3a
Odonno <bottiau.david@laposte.net>
2024-11-14 15:57:08 +0100

    feat: add surrealdb hosting/client integration

---
+++ src/CommunityToolkit.Aspire.Hosting.SurrealDb/SurrealDbContainerImageTags.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Aspire.Hosting;
+
+internal sealed class SurrealDbContainerImageTags
+{
+    public const string Registry = "docker.io";
+    public const string Image = "surrealdb/surrealdb";
+    public const string Tag = "v2.0.4";
+}
