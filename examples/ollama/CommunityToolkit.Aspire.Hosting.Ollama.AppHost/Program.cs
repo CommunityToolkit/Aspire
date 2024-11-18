28c61f6ed1f7331d860d7439851dcafd84d58db6
Aaron Powell <me@aaron-powell.com>
2024-11-18 01:22:49 +0000

    Moving from a lifecycle hook to event for model download
    The lifecycle hook would get blocked if you used WaitFor on the model resource, as it wouldn't fire until the model was healthy, which it couldn't achieve. Refactored to use an event instead that relies on the server resource ready event.
    
    Had to turn the event into a non-blocking with Task.Run otherwise models couldn't be downloaded concurrently, as the events would run one at a time, waiting for model download completion before continuing on.
    
    Also took this time to refactor the AddModel method to have a few extension methods, making it a bit clearer as to what happens when.
    
    Fixes #256

--- examples/ollama/CommunityToolkit.Aspire.Hosting.Ollama.AppHost/Program.cs
+++ examples/ollama/CommunityToolkit.Aspire.Hosting.Ollama.AppHost/Program.cs
@@ -10,6 +10,7 @@ var llama = ollama.AddHuggingFaceModel("llama", "bartowski/Llama-3.2-1B-Instruct
 builder.AddProject<Projects.CommunityToolkit_Aspire_Hosting_Ollama_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(phi3)
+    .WaitFor(phi3)
     .WithReference(llama);
 
 builder.Build().Run();
