d249a9af56521fcc6c58593a503d6d4e4e6df9f9
Aaron Powell <me@aaron-powell.com>
2024-11-19 13:28:21 +1100

    Doc errors fail builds (#259)
    * Doc errors weren't picked up for build issues
    
    Fixes #256
    
    * Fixing docs errors
    
    * Removing java warnings to make VS Code Problems view cleaner

--- src/CommunityToolkit.Aspire.Hosting.Python.Extensions/UvicornAppResource.cs
+++ src/CommunityToolkit.Aspire.Hosting.Python.Extensions/UvicornAppResource.cs
@@ -2,5 +2,10 @@ using Aspire.Hosting.Python;
 
 namespace Aspire.Hosting.ApplicationModel;
 
+/// <summary>
+/// Represents a Uvicorn application.
+/// </summary>
+/// <param name="name">The name of the resource.</param>
+/// <param name="workingDirectory">The working directory for uvicorn.</param>
 public class UvicornAppResource(string name, string workingDirectory)
     : PythonAppResource(name, "uvicorn", workingDirectory), IResourceWithServiceDiscovery;
