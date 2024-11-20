215dddacfa51ae9015900b7b47f3720cbf1d182e
anoordover <arno@noordover.net>
2024-11-20 19:31:45 +0100

    #253 resolved comments

--- examples/activemq/CommunityToolkit.Aspire.Hosting.ActiveMQ.MassTransit/HelloWorldConsumer.cs
+++ examples/activemq/CommunityToolkit.Aspire.Hosting.ActiveMQ.MassTransit/HelloWorldConsumer.cs
@@ -2,18 +2,11 @@ using MassTransit;
 
 namespace CommunityToolkit.Aspire.Hosting.ActiveMQ.MassTransit;
 
-public class HelloWorldConsumer : IConsumer<Message>
+public class HelloWorldConsumer(ILogger<HelloWorldConsumer> logger) : IConsumer<Message>
 {
-    private readonly ILogger<HelloWorldConsumer> _logger;
-
-    public HelloWorldConsumer(ILogger<HelloWorldConsumer> logger)
-    {
-        _logger = logger;
-    }
-
     public Task Consume(ConsumeContext<Message> context)
     {
-        _logger.LogInformation("Received message: {Text}", context.Message.Text);
+        logger.LogInformation("Received message: {Text}", context.Message.Text);
         return Task.CompletedTask;
     }
 }
