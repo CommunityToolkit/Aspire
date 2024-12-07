b8e729469d439d0a2366511a28f9c037a8f9f972
Odonno <bottiau.david@laposte.net>
2024-12-07 11:17:06 +0100

    feat: add Surrealist UI

--- src/CommunityToolkit.Aspire.Hosting.SurrealDb/SurrealDbContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.SurrealDb/SurrealDbContainerImageTags.cs
@@ -5,7 +5,17 @@ namespace Aspire.Hosting;
 
 internal sealed class SurrealDbContainerImageTags
 {
+    /// <summary>docker.io</summary>
     public const string Registry = "docker.io";
+    /// <summary>surrealdb/surrealdb</summary>
     public const string Image = "surrealdb/surrealdb";
-    public const string Tag = "v2.0.4";
+    /// <summary>v2.1</summary>
+    public const string Tag = "v2.1";
+    
+    /// <summary>docker.io</summary>
+    public const string SurrealistRegistry = "docker.io";
+    /// <summary>surrealdb/surrealist</summary>
+    public const string SurrealistImage = "surrealdb/surrealist";
+    /// <summary>3.1.6</summary>
+    public const string SurrealistTag = "3.1.6";
 }
