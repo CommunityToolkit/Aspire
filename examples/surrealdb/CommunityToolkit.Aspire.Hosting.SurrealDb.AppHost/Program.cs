51d80f8ab40022c0a8a30d0d272b30bb8a23189d
Odonno <bottiau.david@laposte.net>
2024-12-07 11:17:06 +0100

    feat: add Surrealist UI

--- examples/surrealdb/CommunityToolkit.Aspire.Hosting.SurrealDb.AppHost/Program.cs
+++ examples/surrealdb/CommunityToolkit.Aspire.Hosting.SurrealDb.AppHost/Program.cs
@@ -3,6 +3,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var db = builder.AddSurrealServer("surreal")
+    .WithSurrealist()
     .AddNamespace("ns")
     .AddDatabase("db");
 
