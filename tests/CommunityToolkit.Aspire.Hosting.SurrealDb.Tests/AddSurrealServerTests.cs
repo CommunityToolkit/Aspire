c413eed6ced881eb394e57392fc6893fa531dd11
Odonno <bottiau.david@laposte.net>
2024-12-10 22:37:00 +0100

    test: fix tests

--- tests/CommunityToolkit.Aspire.Hosting.SurrealDb.Tests/AddSurrealServerTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.SurrealDb.Tests/AddSurrealServerTests.cs
@@ -91,7 +91,7 @@ public class AddSurrealServerTests
         var connectionStringResource = Assert.Single(appModel.Resources.OfType<SurrealDbServerResource>());
         var connectionString = await connectionStringResource.GetConnectionStringAsync(default);
 
-        Assert.Equal("Server=ws://127.0.0.1:8000/rpc;User=root;Password=p@ssw0rd1", connectionString);
+        Assert.Equal("Server=ws://localhost:8000/rpc;User=root;Password=p@ssw0rd1", connectionString);
         Assert.Equal("Server=ws://{surreal.bindings.tcp.host}:{surreal.bindings.tcp.port}/rpc;User=root;Password={pass.value}", connectionStringResource.ConnectionStringExpression.ValueExpression);
     }
 
@@ -116,7 +116,7 @@ public class AddSurrealServerTests
         var connectionStringResource = (IResourceWithConnectionString)surrealResource;
         var connectionString = await connectionStringResource.GetConnectionStringAsync();
 
-        Assert.Equal("Server=ws://127.0.0.1:8000/rpc;User=root;Password=p@ssw0rd1;Namespace=myns;Database=mydb", connectionString);
+        Assert.Equal("Server=ws://localhost:8000/rpc;User=root;Password=p@ssw0rd1;Namespace=myns;Database=mydb", connectionString);
         Assert.Equal("{ns.connectionString};Database=mydb", connectionStringResource.ConnectionStringExpression.ValueExpression);
     }
 
