1845e88d635dbefbc8ec96f1d491416c539706f4
Tommaso Stocchi <tstocchi@microsoft.com>
2024-12-16 12:16:19 +0100

    working uv integration

--- examples/python/CommunityToolkit.Aspire.Hosting.Python.Extensions.AppHost/Program.cs
+++ examples/python/CommunityToolkit.Aspire.Hosting.Python.Extensions.AppHost/Program.cs
@@ -3,4 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var uvicorn = builder.AddUvicornApp("uvicornapp", "../uvicornapp-api", "main:app")
     .WithHttpEndpoint(env: "UVICORN_PORT");
 
+var uv = builder.AddUvApp("uvapp", "../uv-api", "uv-api")
+    .WithHttpEndpoint(env: "PORT");
+
 builder.Build().Run();
