5182536dbd44b89cb0462628f569305b0c21dc75
Aaron Powell <me@aaron-powell.com>
2025-01-08 01:26:20 +0000

    Implementing tests for sqlite client integration

---
+++ tests/CommunityToolkit.Aspire.Microsoft.Data.Sqlite.Tests/ConfigurationTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace CommunityToolkit.Aspire.Microsoft.Data.Sqlite.Tests;
+
+public class ConfigurationTests
+{
+    [Fact]
+    public void ConnectionStringIsNullByDefault() =>
+        Assert.Null(new SqliteClientSettings().ConnectionString);
+
+    [Fact]
+    public void HealthChecksEnabledByDefault() =>
+        Assert.False(new SqliteClientSettings().DisableHealthChecks);
+}
