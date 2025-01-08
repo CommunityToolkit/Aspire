66d7963182741186472b46d13f669a4560f7bd74
Aaron Powell <me@aaron-powell.com>
2025-01-08 01:26:20 +0000

    Implementing tests for sqlite client integration

--- src/CommunityToolkit.Aspire.Microsoft.Data.Sqlite/AspireSqliteExtensions.cs
+++ src/CommunityToolkit.Aspire.Microsoft.Data.Sqlite/AspireSqliteExtensions.cs
@@ -91,11 +91,17 @@ public static class AspireSqliteExtensions
     {
         if (serviceKey is null)
         {
-            builder.Services.AddScoped(sp => new SqliteConnection(settings.ConnectionString));
+            builder.Services.AddScoped(sp => CreateConnection(sp, null));
         }
         else
         {
-            builder.Services.AddKeyedScoped(serviceKey, (_, _) => new SqliteConnection(settings.ConnectionString));
+            builder.Services.AddKeyedScoped(serviceKey, CreateConnection);
+        }
+
+        SqliteConnection CreateConnection(IServiceProvider sp, object? key)
+        {
+            ConnectionStringValidation.ValidateConnectionString(settings.ConnectionString, connectionName, DefaultConfigSectionName);
+            return new SqliteConnection(settings.ConnectionString);
         }
     }
 }
--- src/CommunityToolkit.Aspire.Microsoft.Data.Sqlite/SqliteClientSettings.cs
+++ src/CommunityToolkit.Aspire.Microsoft.Data.Sqlite/SqliteClientSettings.cs
@@ -3,7 +3,7 @@ namespace Microsoft.Extensions.Hosting;
 /// <summary>
 /// Represents the settings for the Sqlite client.
 /// </summary>
-public class SqliteClientSettings
+public sealed class SqliteClientSettings
 {
     /// <summary>
     /// The connection string of the PostgreSQL database to connect to.
@@ -17,9 +17,4 @@ public class SqliteClientSettings
     /// The default value is <see langword="false"/>.
     /// </value>
     public bool DisableHealthChecks { get; set; }
-
-    /// <summary>
-    /// Gets or sets the default timeout for the database operations.
-    /// </summary>
-    public int DefaultTimeout { get; set; }
 }
