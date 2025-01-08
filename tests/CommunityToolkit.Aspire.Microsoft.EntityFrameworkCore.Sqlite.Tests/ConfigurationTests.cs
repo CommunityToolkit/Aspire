b481134c67dd7c3b889fa4c9e41c76c989bd0d8c
Aaron Powell <me@aaron-powell.com>
2025-01-08 03:00:13 +0000

    Adding sqlite ef test set

---
+++ tests/CommunityToolkit.Aspire.Microsoft.EntityFrameworkCore.Sqlite.Tests/ConfigurationTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace CommunityToolkit.Aspire.Microsoft.EntityFrameworkCore.Sqlite.Tests;
+
+public class ConfigurationTests
+{
+    [Fact]
+    public void ConnectionStringIsNullByDefault() =>
+        Assert.Null(new SqliteEntityFrameworkCoreSettings().ConnectionString);
+
+    [Fact]
+    public void HealthChecksEnabledByDefault() =>
+        Assert.False(new SqliteEntityFrameworkCoreSettings().DisableHealthChecks);
+}
