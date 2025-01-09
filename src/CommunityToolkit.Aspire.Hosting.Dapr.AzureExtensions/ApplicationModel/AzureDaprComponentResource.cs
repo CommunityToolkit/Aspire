6a2c466fc46ac61d34559a812e7b78955b349e24
Brett Smith <brett@pukekos.co.nz>
2025-01-09 15:19:06 +1300

    Update src/CommunityToolkit.Aspire.Hosting.Dapr.AzureExtensions/ApplicationModel/AzureDaprComponentResource.cs
    Co-authored-by: Aaron Powell <me@aaron-powell.com>

--- src/CommunityToolkit.Aspire.Hosting.Dapr.AzureExtensions/ApplicationModel/AzureDaprComponentResource.cs
+++ src/CommunityToolkit.Aspire.Hosting.Dapr.AzureExtensions/ApplicationModel/AzureDaprComponentResource.cs
@@ -10,5 +10,4 @@ namespace Aspire.Hosting.ApplicationModel;
 /// </remarks>
 /// <param name="bicepIdentifier">The Bicep identifier.</param>
 /// <param name="configureInfrastructure">The action to configure the Azure resource infrastructure.</param>
-public class AzureDaprComponentResource(string bicepIdentifier,
-    Action<AzureResourceInfrastructure> configureInfrastructure) : AzureProvisioningResource(bicepIdentifier, configureInfrastructure);
+public class AzureDaprComponentResource(string bicepIdentifier, Action<AzureResourceInfrastructure> configureInfrastructure) : AzureProvisioningResource(bicepIdentifier, configureInfrastructure);
