4fb790503718dc7f64c453b6eec7b07e200723de
Aaron Powell <me@aaron-powell.com>
2025-01-21 02:56:02 +0000

    Bypass logging

--- src/CommunityToolkit.Aspire.Hosting.Dapr/DaprDistributedApplicationLifecycleHook.cs
+++ src/CommunityToolkit.Aspire.Hosting.Dapr/DaprDistributedApplicationLifecycleHook.cs
@@ -306,7 +306,7 @@ internal sealed class DaprDistributedApplicationLifecycleHook : IDistributedAppl
         {
             if (File.Exists(path))
             {
-                _logger.LogWarning("Checking path: {Path}", path);
+                Console.WriteLine("Checking path: {Path}", path);
                 return path;
             }
         }
