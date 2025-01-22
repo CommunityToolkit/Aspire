5cf98d8cc41db02d1cc96dee66da7ee63d040abd
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-01-22 15:30:03 -0500

    Set docker tag to 1.4.1

--- src/CommunityToolkit.Aspire.Hosting.ClamAv/ClamAvContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.ClamAv/ClamAvContainerImageTags.cs
@@ -6,7 +6,7 @@ internal static class ClamAvContainerImageTags
     public const string Registry = "docker.io";
     /// <summary>clamav/clamav</summary>
     public const string Image = "clamav/clamav";
-    /// <summary>latest</summary>
-    public const string Tag = "latest";
+    /// <summary>1.4.1</summary>
+    public const string Tag = "1.4.1";
 }
 
