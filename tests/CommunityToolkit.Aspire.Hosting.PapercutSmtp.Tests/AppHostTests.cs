f4bb994a8e711b0c02cb3172521ad6c52abe5cfb
Arno Noordover <anoordover@users.noreply.github.com>
2025-01-27 22:01:03 +0100

    #240 changed unittest on papercut to call papercut directly

--- tests/CommunityToolkit.Aspire.Hosting.PapercutSmtp.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.PapercutSmtp.Tests/AppHostTests.cs
@@ -1,29 +1,30 @@
 using Aspire.Components.Common.Tests;
-using CommunityToolkit.Aspire.Hosting.PapercutSmtp.SendMailApi;
 using CommunityToolkit.Aspire.Testing;
-using System.Net.Http.Json;
+using System.Net.Mail;
 
 namespace CommunityToolkit.Aspire.Hosting.PapercutSmtp.Tests;
 
 [RequiresDocker]
-public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_PapercutSmtp_SendMailApi> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_PapercutSmtp_SendMailApi>>
+public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_PapercutSmtp_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_PapercutSmtp_AppHost>>
 {
     [Fact]
-    public async Task ResourceStartsAndSendingMailRespondsOk()
+    public async Task ResourceStartsAndSendingMailNoException()
     {
-        const string resourceName = "sendmail";
+        const string resourceName = "papercut";
         await fixture.ResourceNotificationService.WaitForResourceHealthyAsync(resourceName).WaitAsync(TimeSpan.FromMinutes(2));
-        HttpClient httpClient = fixture.CreateHttpClient(resourceName, "http");
-
-        MailData payload = new()
-        {
-            Body = "Hello World",
-            From = "from@test.com",
-            To = "to@test.com",
-            Subject = "subject"
-        };
-        HttpResponseMessage response = await httpClient.PostAsync("/send", JsonContent.Create(payload));
-
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        string? connectionString = await fixture.GetConnectionString("papercut");
+        Uri connectionUri = new(connectionString!);
+        SmtpClient smtpClient = new SmtpClient(connectionUri.Host, connectionUri.Port);
+        MailAddress from = new("test@test.com", "TestFromName");
+        MailAddress to = new("to@test.com", "TestToName");
+        MailMessage myMail = new(from, to);
+        MailAddress replyTo = new("reply@test.com");
+        myMail.ReplyToList.Add(replyTo);
+        myMail.Subject = "Subject";
+        myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+        myMail.Body = "Hello world!";
+        myMail.BodyEncoding = System.Text.Encoding.UTF8;
+        Exception? exception = Record.Exception(() => smtpClient.Send(myMail));
+        Assert.Null(exception);
     }
 }
