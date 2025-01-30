61c3aa744cd387ad5dffb7e27dfa6d0a3c9c4a38
Aaron Powell <me@aaron-powell.com>
2025-01-30 01:24:12 +0000

    Loading sqlite extensions from nuget packages

--- src/CommunityToolkit.Aspire.Microsoft.Data.Sqlite/AspireSqliteExtensions.cs
+++ src/CommunityToolkit.Aspire.Microsoft.Data.Sqlite/AspireSqliteExtensions.cs
@@ -3,9 +3,12 @@ using HealthChecks.Sqlite;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Data.Common;
+using System.Runtime.InteropServices;
 using System.Text.Json;
+using RuntimeEnvironment = Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment;
 
 namespace Microsoft.Extensions.Hosting;
 
@@ -116,10 +119,103 @@ public static class AspireSqliteExtensions
 
             foreach (var extension in settings.Extensions)
             {
+                EnsureLoadable(extension, extension);
                 connection.LoadExtension(extension);
             }
 
             return connection;
+        }
+    }
+
+    // Adapted from https://github.com/dotnet/docs/blob/dbbeda13bf016a6ff76b0baab1488c927a64ff24/samples/snippets/standard/data/sqlite/ExtensionsSample/Program.cs#L40
+    internal static void EnsureLoadable(string package, string library)
+    {
+        var runtimeLibrary = DependencyContext.Default?.RuntimeLibraries.FirstOrDefault(l => l.Name == package);
+        if (runtimeLibrary is null)
+            return;
+
+        string sharedLibraryExtension;
+        string pathVariableName = "PATH";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            sharedLibraryExtension = ".dll";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            sharedLibraryExtension = ".so";
+            pathVariableName = "LD_LIBRARY_PATH";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            sharedLibraryExtension = ".dylib";
+            pathVariableName = "DYLD_LIBRARY_PATH";
+        }
+        else
+        {
+            throw new NotSupportedException("Unsupported OS platform");
+        }
+
+        var candidateAssets = new Dictionary<(string? Package, string Asset), int>();
+        var rid = RuntimeEnvironment.GetRuntimeIdentifier();
+        var rids = DependencyContext.Default?.RuntimeGraph.First(g => g.Runtime == rid).Fallbacks.ToList() ?? [];
+        rids.Insert(0, rid);
+
+        foreach (var group in runtimeLibrary.NativeLibraryGroups)
+        {
+            foreach (var file in group.RuntimeFiles)
+            {
+                if (string.Equals(
+                    Path.GetFileName(file.Path),
+                    library + sharedLibraryExtension,
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    var fallbacks = rids.IndexOf(group.Runtime);
+                    if (fallbacks != -1)
+                    {
+                        candidateAssets.Add((runtimeLibrary.Path, file.Path), fallbacks);
+                    }
+                }
+            }
+        }
+
+        var assetPath = candidateAssets
+            .OrderBy(p => p.Value)
+            .Select(p => p.Key)
+            .FirstOrDefault();
+        if (assetPath != default)
+        {
+            string? assetDirectory = null;
+            if (File.Exists(Path.Combine(AppContext.BaseDirectory, assetPath.Asset)))
+            {
+                // NB: Framework-dependent deployments copy assets to the application base directory
+                assetDirectory = Path.Combine(
+                    AppContext.BaseDirectory,
+                    Path.GetDirectoryName(assetPath.Asset.Replace('/', Path.DirectorySeparatorChar))!);
+            }
+            else
+            {
+                string? assetFullPath = null;
+                var probingDirectories = ((string?)AppDomain.CurrentDomain.GetData("PROBING_DIRECTORIES"))?
+                    .Split(Path.PathSeparator) ?? [];
+                foreach (var directory in probingDirectories)
+                {
+                    var candidateFullPath = Path.Combine(
+                        directory,
+                        assetPath.Package ?? "",
+                        assetPath.Asset);
+                    if (File.Exists(candidateFullPath))
+                    {
+                        assetFullPath = candidateFullPath;
+                    }
+                }
+
+                assetDirectory = Path.GetDirectoryName(assetFullPath);
+            }
+
+            var path = new HashSet<string>(Environment.GetEnvironmentVariable(pathVariableName)!.Split(Path.PathSeparator));
+
+            if (assetDirectory is not null && path.Add(assetDirectory))
+                Environment.SetEnvironmentVariable(pathVariableName, string.Join(Path.PathSeparator, path));
         }
     }
 }
