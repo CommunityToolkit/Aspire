199b90f450047dbc4872423e94dbd58c0a352f1b
Aaron Powell <me@aaron-powell.com>
2025-01-31 14:06:15 +1100

    Dapr migration (#408)
    * Dapr migration initial import (#378)
    
    * Dropping files from Aspire repo
    
    * Moving to the right namespace/folder naming
    
    * Adding to solution and getting it to compile
    
    * Adding in the Dapr tests from the Aspire repo
    
    Had to remove the DaprSchemaTests file as we can't do schema tests (missing a lot of infrastructure from the Aspire repo).
    
    Had to edit the DaprTests to not use EnvironmentVariableEvaluator, which we can't leverage as it uses some internal types from Aspire.Hosting. This means that our testing of the environment variables is slightly different, and the values we assert against are not the docker internal host endpoints, but the public endpoints
    
    * Dapr azure hosting ext (#371)
    
    * Create Dap Azure extensions project
    Create Dapr Azure Redis project
    Create Example AppHost + ApiService
    
    * Work in progress
    Create Dapr resource for provisioning
    
    * Remove specific resource as not generating properly
    Use AddAzureInfrastructure
    
    * Updated to use secret refs
    
    * Fix - Remove code used for testing
    Add AzureDaprComponentResource
    Start of unit tests
    
    * Unit tests +
    Fixes based on unit tests
    
    * remove bicep file
    
    * Tests for AzureRedis
    
    * Add Readme and perform small cleanup tasks
    
    * Update src/CommunityToolkit.Aspire.Hosting.Dapr.AzureExtensions/ApplicationModel/AzureDaprComponentResource.cs
    
    Co-authored-by: Aaron Powell <me@aaron-powell.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Aaron Powell <me@aaron-powell.com>
    
    * Revert unintentional change to Java.AppHost
    
    * Update tests/CommunityToolkit.Aspire.Hosting.Dapr.AzureRedis.Tests/ResourceCreationTests.cs
    
    Co-authored-by: Aaron Powell <me@aaron-powell.com>
    
    * Updated azure redis documentation also added missing xmldocs for api
    
    * correct unit test approach
    
    * remove unnecessary comment
    
    * null checking
    
    * More null checking
    
    * Update Azure redis readme
    
    * Update AzureExtension readme
    
    * move extensions to shared files
    remove Dapr Azure extensions project
    make dapr azure extensions internal
    include extensions in dapr redis package
    include extensions in dapr extensions tests
    make dapr redis internals visible to dapr redis tests
    
    * change redisCache to infra rename source to redisBuilder
    
    ---------
    
    Co-authored-by: Aaron Powell <me@aaron-powell.com>
    
    * Fixing broken sln
    
    * Dapr example and tests (#394)
    
    * Migrating sample from dotnet/aspire repo
    
    * Adding dapr tests
    
    * Adding dapr setup to CI workflow
    
    * Following the setup-dapr instructions
    
    * Debugging CI
    
    * Falling back to looking at PATH
    
    * Fixing line endings replacement
    
    * Added PATH lookup to Windows
    
    * Adding some diagnostic info
    
    * Changing log level
    
    * init was only running on linux, which I think is wrong
    
    * Turning up logging
    
    * Bypass logging
    
    * Interpolated strings
    
    * Adding resource logger service
    
    * Java app build extension (#348)
    
    * Adding the ability to do a maven build in the app host
    
    This uses an event to run the mvnw build (or it can be customised via options), so it is only used when the app host is running the resource
    
    Fixes #339
    
    * Adding a 'build with maven' command
    
    Allows you to rebuild the java app without having to restart the whole app host.
    
    * Expanding test coverage
    
    * Some more tests
    
    * Windows exe needs a file extension
    
    * Rolling back some changes
    
    * Renaming step
    
    * Tidying up the tests
    
    * Requiring docker for dapr tests
    
    * Adding codeowners
    
    * Moving dapr extensions to use our dapr package
    
    * Forgot to update the tests project
    
    * Adding some assembly filters
    
    * Adding readme updates
    
    * Slight break in the markdown
    
    ---------
    
    Co-authored-by: Brett Smith <brett@pukekos.co.nz>

---
+++ tests/CommunityToolkit.Aspire.Hosting.Dapr.Tests/AppHostTests.cs
@@ -0,0 +1,21 @@
+using Aspire.Components.Common.Tests;
+using CommunityToolkit.Aspire.Testing;
+
+namespace CommunityToolkit.Aspire.Hosting.Dapr.Tests;
+
+[RequiresDocker]
+public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Dapr_AppHost> fixture) :
+    IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Dapr_AppHost>>
+{
+    [Fact]
+    public async Task ResourceStartsAndRespondsOk()
+    {
+        var resourceName = "servicea";
+        await fixture.ResourceNotificationService.WaitForResourceHealthyAsync(resourceName).WaitAsync(TimeSpan.FromMinutes(5));
+        var httpClient = fixture.CreateHttpClient(resourceName);
+
+        var response = await httpClient.GetAsync("/weatherforecast");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
