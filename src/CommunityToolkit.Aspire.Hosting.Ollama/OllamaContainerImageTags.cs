e2b7adb7429eb12810ea0a9c88b6a849811ed63b
Aaron Powell <me@aaron-powell.com>
2025-01-31 03:12:05 +0000

    Updating ollama image tag

--- src/CommunityToolkit.Aspire.Hosting.Ollama/OllamaContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.Ollama/OllamaContainerImageTags.cs
@@ -4,7 +4,7 @@ internal static class OllamaContainerImageTags
 {
     public const string Registry = "docker.io";
     public const string Image = "ollama/ollama";
-    public const string Tag = "0.5.1";
+    public const string Tag = "0.5.7";
 
     public const string OpenWebUIRegistry = "ghcr.io";
     public const string OpenWebUIImage = "open-webui/open-webui";
