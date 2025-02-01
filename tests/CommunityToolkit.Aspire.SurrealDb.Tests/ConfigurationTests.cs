0f79dd11e43a0f940859c41192e85b67ff9e3741
Odonno <bottiau.david@laposte.net>
2025-02-01 12:37:18 +0100

    test: fix tests

--- tests/CommunityToolkit.Aspire.SurrealDb.Tests/ConfigurationTests.cs
+++ tests/CommunityToolkit.Aspire.SurrealDb.Tests/ConfigurationTests.cs
@@ -1,27 +1,19 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using FluentAssertions;
-
 namespace CommunityToolkit.Aspire.SurrealDb.Tests;
 
 public class ConfigurationTests
 {
     [Fact]
-    public void HealthChecksEnabledByDefault()
-    {
-        new SurrealDbClientSettings().DisableHealthChecks.Should().BeFalse();
-    }
+    public void HealthChecksEnabledByDefault() =>
+        Assert.False(new SurrealDbClientSettings().DisableHealthChecks);
 
     [Fact]
-    public void OptionsAreNullByDefault()
-    {
-        new SurrealDbClientSettings().Options.Should().BeNull();
-    }
-    
+    public void OptionsAreNullByDefault() =>
+        Assert.Null(new SurrealDbClientSettings().Options);
+
     [Fact]
-    public void LifetimeIsSingletonByDefault()
-    {
-        new SurrealDbClientSettings().Lifetime.Should().Be(ServiceLifetime.Singleton);
-    }
+    public void LifetimeIsSingletonByDefault() =>
+        Assert.Equal(ServiceLifetime.Singleton, new SurrealDbClientSettings().Lifetime);
 }
