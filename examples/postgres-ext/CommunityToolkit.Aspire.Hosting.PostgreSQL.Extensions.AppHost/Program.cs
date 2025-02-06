b46dff8b24b893bd45f77d54effea0ad6b43ca92
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2025-02-07 00:05:26 +0330

    Add DbGate support for MongoDB

--- examples/postgres-ext/CommunityToolkit.Aspire.Hosting.PostgreSQL.Extensions.AppHost/Program.cs
+++ examples/postgres-ext/CommunityToolkit.Aspire.Hosting.PostgreSQL.Extensions.AppHost/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres1 = builder.AddPostgres("postgres1")
-    .WithDbGate(c=> c.WithHostPort(8068));
+    .WithDbGate(c => c.WithHostPort(8068));
 postgres1.AddDatabase("db1");
 postgres1.AddDatabase("db2");
 
@@ -9,5 +9,8 @@ var postgres2 = builder.AddPostgres("postgres2")
     .WithDbGate(c => c.WithHostPort(8068));
 postgres2.AddDatabase("db3");
 postgres2.AddDatabase("db4");
+
+var mongodb1 = builder.AddMongoDB("mongodb1").WithDbGate();
+mongodb1.AddDatabase("db5");
 
 builder.Build().Run();
