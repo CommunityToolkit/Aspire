537fc3dbcaf1999429bc3d000cf24f1e6f538317
David Sekula <dave@simcube.co.uk>
2025-02-19 21:04:17 +0000

    feat: add MailPit support #476 (#479)

---
+++ src/CommunityToolkit.Aspire.Hosting.MailPit/MailPitContainerImageTags.cs
@@ -0,0 +1,13 @@
+namespace CommunityToolkit.Aspire.Hosting.MailPit;
+
+internal static class MailPitContainerImageTags
+{
+    /// <remarks>docker.io</remarks>
+    public const string Registry = "docker.io";
+
+    /// <remarks>axllent/mailpit</remarks>
+    public const string Image = "axllent/mailpit";
+
+    /// <remarks>v1.22.3</remarks>
+    public const string Tag = "v1.22.3";
+}
---
+++ src/CommunityToolkit.Aspire.Hosting.MailPit/MailPitContainerResource.cs
@@ -0,0 +1,23 @@
+﻿namespace Aspire.Hosting.ApplicationModel;
+
+/// <summary>
+/// Resource for the MailPit server.
+/// </summary>
+/// <param name="name"></param>
+public class MailPitContainerResource(string name) : ContainerResource(name), IResourceWithConnectionString
+{
+    internal const int HttpEndpointPort = 8025;
+    internal const int SmtpEndpointPort = 1025;
+    internal const string SmtpEndpointName = "smtp";
+    internal const string HttpEndpointName = "http";
+    internal const string DatabaseEnvVar = "MP_DATABASE";
+
+    private EndpointReference? _smtpEndpoint;
+    private EndpointReference SmtpEndpoint => _smtpEndpoint ??= new EndpointReference(this, SmtpEndpointName);
+
+    /// <summary>
+    /// ConnectionString for MailPit smtp endpoint in the form of smtp://host:port.
+    /// </summary>
+    public ReferenceExpression ConnectionStringExpression => ReferenceExpression.Create(
+        $"Endpoint={SmtpEndpoint.Scheme}://{SmtpEndpoint.Property(EndpointProperty.Host)}:{SmtpEndpoint.Property(EndpointProperty.Port)}");
+}
