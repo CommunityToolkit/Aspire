94066646753b4bc3456a06a0b2290b5e8a4f6ea2
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2025-02-28 14:59:17 +0330

    Adding workflow to automatically compare public API surface against previous release

---
+++ eng/apicompat/generate-api-diffs.cmd
@@ -0,0 +1,3 @@
+for /r src %%i in (*.csproj) do (  
+    dotnet build "%%i" -f net8.0 --configuration Release --no-incremental /t:"Build;GenAPIGenerateReferenceAssemblySource"  
+)  
---
+++ eng/apicompat/generate-api-diffs.sh
@@ -0,0 +1,3 @@
+find src -type f -name "*.csproj" | while read -r csproj; do
+    dotnet build "$csproj" -f net8.0 --configuration Release --no-incremental -t:Build -t:GenAPIGenerateReferenceAssemblySource
+done
