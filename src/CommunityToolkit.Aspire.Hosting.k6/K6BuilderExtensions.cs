13b678c5047b9921303b86a1ef1f981fdc6fe9c6
Odonno <bottiau.david@laposte.net>
2025-03-25 12:32:25 +0100

    feat: create k6 hosting integration

---
+++ src/CommunityToolkit.Aspire.Hosting.k6/K6BuilderExtensions.cs
@@ -0,0 +1,61 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.ApplicationModel;
+using CommunityToolkit.Aspire.Hosting.k6;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// Provides extension methods for adding Grafana k6 resources to the application model.
+/// </summary>
+public static class K6BuilderExtensions
+{
+    private const int K6Port = 6565;
+
+    /// <summary>
+    /// Adds a Grafana k6 container resource to the application model.
+    /// The default image is <inheritdoc cref="K6ContainerImageTags.Image"/> and the tag is <inheritdoc cref="K6ContainerImageTags.Tag"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
+    /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
+    /// <param name="enableBrowserExtensions">Enables browser automation and end-to-end web testing to k6. <see href="https://grafana.com/docs/k6/latest/using-k6-browser/"/></param>
+    /// <param name="port">The host port to bind the underlying container to.</param>
+    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <remarks>
+    /// <example>
+    /// Add an Grafana k6 container to the application model and reference it in a .NET project.
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    /// 
+    /// var api = builder.AddProject&lt;Projects.Api&gt;("api")
+    /// var k6 = builder.AddK6("k6");
+    ///     .WithReference(api);
+    ///  
+    /// builder.Build().Run(); 
+    /// </code>
+    /// </example>
+    /// </remarks>
+    public static IResourceBuilder<K6Resource> AddK6(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        bool enableBrowserExtensions = false,
+        int? port = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(name);
+
+        var k6Resource = new K6Resource(name);
+        
+        string tag = enableBrowserExtensions
+            ? $"{K6ContainerImageTags.Tag}-with-browser"
+            : K6ContainerImageTags.Tag;
+
+        return builder.AddResource(k6Resource)
+            .WithImage(K6ContainerImageTags.Image, tag)
+            .WithImageRegistry(K6ContainerImageTags.Registry)
+            .WithHttpEndpoint(targetPort: K6Port, port: port, name: K6Resource.PrimaryEndpointName)
+            .WithHttpHealthCheck("/health")
+            .WithOtlpExporter();
+    }
+}
