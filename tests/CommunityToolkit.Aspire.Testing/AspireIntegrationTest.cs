39ae62554a70e7b1e6a0717b53732ab907c51006
Aaron Powell <me@aaron-powell.com>
2025-03-27 11:38:08 +1100

    custom registry for ci (#601)
    * Allowing the override of the container registry for CI
    
    * Updating workflows to set the custom registry
    
    * Adding env for dotnet test run
    
    * only overriding docker.io

--- tests/CommunityToolkit.Aspire.Testing/AspireIntegrationTest.cs
+++ tests/CommunityToolkit.Aspire.Testing/AspireIntegrationTest.cs
@@ -1,5 +1,6 @@
 ﻿using Aspire.Components.Common.Tests;
 using Aspire.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace CommunityToolkit.Aspire.Testing;
@@ -27,6 +28,36 @@ public class AspireIntegrationTestFixture<TEntryPoint>() : DistributedApplicatio
                     builder.SetMinimumLevel(LogLevel.Information);
             })
             .ConfigureHttpClientDefaults(clientBuilder => clientBuilder.AddStandardResilienceHandler());
+
+        if (Environment.GetEnvironmentVariable("CUSTOM_CONTAINER_REGISTRY") is not null)
+        {
+            // We can't use the built-in Aspire container override feature because that changes the registry for all
+            // containers, and we only want to change the registry for the default Docker Hub registry. So we need to
+            // override the registry in the BeforeStartEvent handler (essentially implementing a subset of the built-in
+            // functionality ourselves).
+            applicationBuilder.Eventing.Subscribe<BeforeStartEvent>((@event, ct) =>
+            {
+                var resourcesWithContainerImages = @event.Model.Resources.SelectMany(
+                    r => r.Annotations.OfType<ContainerImageAnnotation>()
+                                      .Select(cia => new { Resource = r, Annotation = cia })
+                    );
+
+                foreach (var resourceWithContainerImage in resourcesWithContainerImages)
+                {
+                    string? dockerHubOverride = Environment.GetEnvironmentVariable("CUSTOM_CONTAINER_REGISTRY");
+
+                    // We only override the registry if the resource is using the default Docker Hub registry, as that is
+                    // rate limited. Microsoft Artifact Registry and GitHub Container Registry are not rate limited, or at
+                    // least we don't pull enough images to hit the rate limit.
+                    if (dockerHubOverride is not null && resourceWithContainerImage.Annotation.Registry == "docker.io")
+                    {
+                        resourceWithContainerImage.Annotation.Registry = dockerHubOverride;
+                    }
+                }
+
+                return Task.CompletedTask;
+            });
+        }
 
         base.OnBuilderCreated(applicationBuilder);
     }
