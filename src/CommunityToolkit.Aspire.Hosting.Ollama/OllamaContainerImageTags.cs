16e091fcfca3432e97fe6100e3aa876236da58db
Alireza Baloochi <Dev.AlirezaBaloochi@gmail.com>
2025-05-07 22:59:11 +0330

    Update ollama tag (#676)

--- src/CommunityToolkit.Aspire.Hosting.Ollama/OllamaContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.Ollama/OllamaContainerImageTags.cs
@@ -4,7 +4,7 @@ internal static class OllamaContainerImageTags
 {
     public const string Registry = "docker.io";
     public const string Image = "ollama/ollama";
-    public const string Tag = "0.6.0";
+    public const string Tag = "0.6.8";
 
     public const string OpenWebUIRegistry = "ghcr.io";
     public const string OpenWebUIImage = "open-webui/open-webui";
