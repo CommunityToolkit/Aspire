f533841fa0ca105d72ea518440a37bfb94b44ff9
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2025-05-16 15:47:03 +0330

    Add Adminer support for MySql

---
+++ src/CommunityToolkit.Aspire.Hosting.MySql.Extensions/MySqlBuilderExtensions.cs
@@ -0,0 +1,92 @@
+﻿using Aspire.Hosting.ApplicationModel;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// Provides extension methods for adding MySql resources to an <see cref="IDistributedApplicationBuilder"/>.
+/// </summary>
+public static class MySqlBuilderExtensions
+{
+    /// <summary>
+    /// Adds an administration and development platform for MySql to the application model using Adminer.
+    /// </summary>
+    /// <remarks>
+    /// This version of the package defaults to the <inheritdoc cref="AdminerContainerImageTags.Tag"/> tag of the <inheritdoc cref="AdminerContainerImageTags.Image"/> container image.
+    /// <param name="builder">The MySql server resource builder.</param>
+    /// <param name="configureContainer">Configuration callback for Adminer container resource.</param>
+    /// <param name="containerName">The name of the container (Optional).</param>
+    /// <example>
+    /// Use in application host with a MySql resource
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    ///
+    /// var mysql = builder.AddMySql("mysql")
+    ///    .WithAdminer();
+    /// var db = mysql.AddDatabase("db");
+    ///
+    /// var api = builder.AddProject&lt;Projects.Api&gt;("api")
+    ///   .WithReference(db);
+    ///
+    /// builder.Build().Run();
+    /// </code>
+    /// </example>
+    /// </remarks>
+    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    public static IResourceBuilder<MySqlServerResource> WithAdminer(this IResourceBuilder<MySqlServerResource> builder, Action<IResourceBuilder<AdminerContainerResource>>? configureContainer = null, string? containerName = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        containerName ??= $"{builder.Resource.Name}-adminer";
+        var adminerBuilder = AdminerBuilderExtensions.AddAdminer(builder.ApplicationBuilder, containerName);
+
+        adminerBuilder
+            .WithEnvironment(context => ConfigureAdminerContainer(context, builder.ApplicationBuilder));
+
+        configureContainer?.Invoke(adminerBuilder);
+
+        return builder;
+    }
+
+    internal static void ConfigureAdminerContainer(EnvironmentCallbackContext context, IDistributedApplicationBuilder applicationBuilder)
+    {
+        var mysqlInstances = applicationBuilder.Resources.OfType<MySqlServerResource>();
+
+        string ADMINER_SERVERS = context.EnvironmentVariables.GetValueOrDefault("ADMINER_SERVERS")?.ToString() ?? string.Empty;
+
+        var new_servers = mysqlInstances.ToDictionary(
+             mysqlServer => mysqlServer.Name,
+             mysqlServer =>
+             {
+                 return new AdminerLoginServer
+                 {
+                     Server = mysqlServer.Name,
+                     UserName = "root",
+                     Password = mysqlServer.PasswordParameter.Value,
+                     Driver = "server" // driver for MySQL is called 'server'
+                 };
+             });
+
+        if (string.IsNullOrEmpty(ADMINER_SERVERS))
+        {
+            string servers_json = JsonSerializer.Serialize(new_servers);
+            context.EnvironmentVariables["ADMINER_SERVERS"] = servers_json;
+        }
+        else
+        {
+            var servers = JsonSerializer.Deserialize<Dictionary<string, AdminerLoginServer>>(ADMINER_SERVERS) ?? throw new InvalidOperationException("The servers should not be null. This should never happen.");
+            foreach (var server in new_servers)
+            {
+                if (!servers.ContainsKey(server.Key))
+                {
+                    servers!.Add(server.Key, server.Value);
+                }
+            }
+            string servers_json = JsonSerializer.Serialize(servers);
+            context.EnvironmentVariables["ADMINER_SERVERS"] = servers_json;
+        }
+
+    }
+}
---
+++ tests/CommunityToolkit.Aspire.Hosting.MySql.Extensions.Tests/ResourceCreationTests.cs
@@ -0,0 +1,162 @@
+using Aspire.Hosting;
+using System.Text.Json;
+
+namespace CommunityToolkit.Aspire.Hosting.MySql.Extensions.Tests;
+
+public class ResourceCreationTests
+{
+    [Fact]
+    public async Task WithAdminerAddsAnnotations()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+
+        var mysqlResourceBuilder = builder.AddMySql("mysql")
+            .WithAdminer();
+
+        var mysqlResource = mysqlResourceBuilder.Resource;
+
+        using var app = builder.Build();
+
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+
+        var adminerResource = appModel.Resources.OfType<AdminerContainerResource>().SingleOrDefault();
+
+        Assert.NotNull(adminerResource);
+
+        Assert.Equal("mysql-adminer", adminerResource.Name);
+
+        var envs = await adminerResource.GetEnvironmentVariableValuesAsync();
+
+        Assert.NotEmpty(envs);
+
+        var servers = new Dictionary<string, AdminerLoginServer>
+        {
+            {
+                "mysql",
+                new AdminerLoginServer
+                {
+                    Driver = "server",
+                    Server = mysqlResource.Name,
+                    Password = mysqlResource.PasswordParameter.Value,
+                    UserName = "root"
+                }
+            },
+        };
+
+        var envValue = JsonSerializer.Serialize(servers);
+        var item = Assert.Single(envs);
+        Assert.Equal("ADMINER_SERVERS", item.Key);
+        Assert.Equal(envValue, item.Value);
+    }
+
+    [Fact]
+    public void MultipleWithAdminerCallsAddsOneAdminerResource()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+        builder.AddMySql("mysql1").WithAdminer();
+        builder.AddMySql("mysql2").WithAdminer();
+
+        using var app = builder.Build();
+
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+
+        var adminerContainer = appModel.Resources.OfType<AdminerContainerResource>().SingleOrDefault();
+        Assert.NotNull(adminerContainer);
+
+        Assert.Equal("mysql1-adminer", adminerContainer.Name);
+    }
+
+    [Fact]
+    public void WithAdminerShouldChangeAdminerHostPort()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+        var mysqlResourceBuilder = builder.AddMySql("mysql")
+            .WithAdminer(c => c.WithHostPort(8068));
+
+        using var app = builder.Build();
+
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+
+        var adminerContainer = appModel.Resources.OfType<AdminerContainerResource>().SingleOrDefault();
+        Assert.NotNull(adminerContainer);
+
+        var primaryEndpoint = adminerContainer.Annotations.OfType<EndpointAnnotation>().Single();
+        Assert.Equal(8068, primaryEndpoint.Port);
+    }
+
+    [Fact]
+    public void WithAdminerShouldChangeAdminerContainerImageTag()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+        var mysqlResourceBuilder = builder.AddMySql("mysql")
+            .WithAdminer(c => c.WithImageTag("manualTag"));
+        using var app = builder.Build();
+
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+
+        var adminerContainer = appModel.Resources.OfType<AdminerContainerResource>().SingleOrDefault();
+        Assert.NotNull(adminerContainer);
+
+        var containerImageAnnotation = adminerContainer.Annotations.OfType<ContainerImageAnnotation>().Single();
+        Assert.Equal("manualTag", containerImageAnnotation.Tag);
+    }
+
+    [Fact]
+    public async Task WithAdminerAddsAnnotationsForMultipleMySqlResource()
+    {
+        var builder = DistributedApplication.CreateBuilder();
+
+        var mysqlResourceBuilder1 = builder.AddMySql("mysql1")
+            .WithAdminer();
+
+        var mysqlResource1 = mysqlResourceBuilder1.Resource;
+
+        var mysqlResourceBuilder2 = builder.AddMySql("mysql2")
+            .WithAdminer();
+
+        var mysqlResource2 = mysqlResourceBuilder2.Resource;
+
+        using var app = builder.Build();
+
+        var appModel = app.Services.GetRequiredService<DistributedApplicationModel>();
+
+        var adminerContainer = appModel.Resources.OfType<AdminerContainerResource>().SingleOrDefault();
+
+        Assert.NotNull(adminerContainer);
+
+        Assert.Equal("mysql1-adminer", adminerContainer.Name);
+
+        var envs = await adminerContainer.GetEnvironmentVariableValuesAsync();
+
+        Assert.NotEmpty(envs);
+
+        var servers = new Dictionary<string, AdminerLoginServer>
+        {
+            {
+                "mysql1",
+                new AdminerLoginServer
+                {
+                    Driver = "server",
+                    Server = mysqlResource1.Name,
+                    Password = mysqlResource1.PasswordParameter.Value,
+                    UserName = "root"
+                }
+            },
+            {
+                "mysql2",
+                new AdminerLoginServer
+                {
+                    Driver = "server",
+                    Server = mysqlResource2.Name,
+                    Password = mysqlResource2.PasswordParameter.Value,
+                    UserName = "root"
+                }
+            }
+        };
+
+        var envValue = JsonSerializer.Serialize(servers);
+        var item = Assert.Single(envs);
+        Assert.Equal("ADMINER_SERVERS", item.Key);
+        Assert.Equal(envValue, item.Value);
+    }
+}
