c9ce63a258b1041c1f77875a68ad477a9a6d3b3d
Aaron Powell <me@aaron-powell.com>
2025-05-24 14:08:34 -0700

    Commenting out the tests completely
    This precents the test app host from actually starting up

--- tests/CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps.Tests/SwaHostingComponentTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps.Tests/SwaHostingComponentTests.cs
@@ -1,41 +1,41 @@
-using CommunityToolkit.Aspire.Testing;
-using System.Net.Http.Json;
+//using CommunityToolkit.Aspire.Testing;
+//using System.Net.Http.Json;
 
-namespace CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps.Tests;
+//namespace CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps.Tests;
 
-public class SwaHostingComponentTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_StaticWebApps_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_StaticWebApps_AppHost>>
-{
-    [Fact(Skip = "Test is unstable in CI")]
-    public async Task CanAccessFrontendSuccessfully()
-    {
-        var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
+//public class SwaHostingComponentTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_StaticWebApps_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_StaticWebApps_AppHost>>
+//{
+//    [Fact(Skip = "Test is unstable in CI")]
+//    public async Task CanAccessFrontendSuccessfully()
+//    {
+//        var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
 
-        var httpClient = fixture.CreateHttpClient("swa");
+//        var httpClient = fixture.CreateHttpClient("swa");
 
-        var ct = cts.Token;
-        await fixture.ResourceNotificationService.WaitForResourceHealthyAsync("swa", ct);
+//        var ct = cts.Token;
+//        await fixture.ResourceNotificationService.WaitForResourceHealthyAsync("swa", ct);
 
-        var response = await httpClient.GetAsync("/", ct);
+//        var response = await httpClient.GetAsync("/", ct);
 
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
+//        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+//    }
 
-    [Fact(Skip = "Test is unstable in CI")]
-    public async Task CanAccessApiSuccessfully()
-    {
-        var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
-        var httpClient = fixture.CreateHttpClient("swa");
+//    [Fact(Skip = "Test is unstable in CI")]
+//    public async Task CanAccessApiSuccessfully()
+//    {
+//        var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
+//        var httpClient = fixture.CreateHttpClient("swa");
 
-        var ct = cts.Token;
-        await fixture.ResourceNotificationService.WaitForResourceHealthyAsync("swa", ct);
+//        var ct = cts.Token;
+//        await fixture.ResourceNotificationService.WaitForResourceHealthyAsync("swa", ct);
 
-        var response = await httpClient.GetAsync("/api/weather", ct);
+//        var response = await httpClient.GetAsync("/api/weather", ct);
 
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        var forecasts = await response.Content.ReadFromJsonAsync<WeatherForecast[]>(ct);
-        Assert.NotNull(forecasts);
-        Assert.Equal(6, forecasts.Length);
-    }
+//        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+//        var forecasts = await response.Content.ReadFromJsonAsync<WeatherForecast[]>(ct);
+//        Assert.NotNull(forecasts);
+//        Assert.Equal(6, forecasts.Length);
+//    }
 
-    record WeatherForecast(DateTime Date, int TemperatureC, string Summary);
-}
+//    record WeatherForecast(DateTime Date, int TemperatureC, string Summary);
+//}
