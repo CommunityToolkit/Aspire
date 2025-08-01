fbe2c55c6e7b56177314569682fcc90276605adc
Odonno <bottiau.david@laposte.net>
2025-08-01 12:45:35 +0200

    feat(surrealdb): add `WithInitFiles` method

--- tests/CommunityToolkit.Aspire.Hosting.SurrealDb.Tests/SurrealDbPublicApiTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.SurrealDb.Tests/SurrealDbPublicApiTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Hosting;
+using Aspire.Hosting.Utils;
 
 namespace CommunityToolkit.Aspire.Hosting.SurrealDb.Tests;
 
@@ -22,7 +23,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void AddSurrealServerContainerShouldThrowWhenNameIsNull()
     {
-        var builder = DistributedApplication.CreateBuilder([]);
+        var builder = TestDistributedApplicationBuilder.Create();
         string name = null!;
 
         var action = () => builder.AddSurrealServer(name);
@@ -46,7 +47,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void AddDatabaseShouldThrowWhenNameIsNull()
     {
-        var builder = DistributedApplication.CreateBuilder([])
+        var builder = TestDistributedApplicationBuilder.Create()
             .AddSurrealServer("surreal")
             .AddNamespace("ns");
         string name = null!;
@@ -60,7 +61,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void AddDatabaseShouldThrowWhenNameIsEmpty()
     {
-        var builder = DistributedApplication.CreateBuilder([])
+        var builder = TestDistributedApplicationBuilder.Create()
             .AddSurrealServer("surreal")
             .AddNamespace("ns");
         string name = "";
@@ -109,7 +110,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void WithDataBindMountShouldThrowWhenSourceIsNull()
     {
-        var builder = new DistributedApplicationBuilder([]);
+        var builder = TestDistributedApplicationBuilder.Create();
         var resourceBuilder = builder.AddSurrealServer("surreal");
     
         string source = null!;
@@ -119,11 +120,52 @@ public class SurrealDbPublicApiTests
         var exception = Assert.Throws<ArgumentNullException>(action);
         Assert.Equal(nameof(source), exception.ParamName);
     }
+    
+    
+    [Fact]
+    public void WithInitFilesShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<SurrealDbServerResource> builder = null!;
+        const string source = "/surrealdb/init.sql";
+
+        var action = () => builder.WithInitFiles(source);
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void WithInitFilesShouldThrowWhenSourceIsNullOrEmpty(bool isNull)
+    {
+        var builder = TestDistributedApplicationBuilder.Create()
+            .AddSurrealServer("surreal");
+        var source = isNull ? null! : string.Empty;
+
+        var action = () => builder.WithInitFiles(source);
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(source), exception.ParamName);
+    }
+
+    [Fact]
+    public void WithSurrealistShouldThrowWhenBuilderIsNull()
+    {
+        IResourceBuilder<SurrealDbServerResource> builder = null!;
+
+        var action = () => builder.WithSurrealist();
+
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal(nameof(builder), exception.ParamName);
+    }
 
     [Fact]
     public void CtorSurrealServerServerResourceShouldThrowWhenNameIsNull()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
         string name = null!;
         const string key = nameof(key);
         var password = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, key, special: false);
@@ -137,7 +179,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerDatabaseResourceShouldThrowWhenNameIsNull()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
 
         string name = null!;
         string namespaceName = "ns1";
@@ -154,7 +196,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerDatabaseResourceShouldThrowWhenNameIsEmpty()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
 
         string name = "";
         string namespaceName = "ns1";
@@ -171,7 +213,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerNamespaceResourceShouldThrowWhenNamespaceNameIsNull()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
 
         string namespaceName = null!;
         var password = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, "password", special: false);
@@ -185,7 +227,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerNamespaceResourceShouldThrowWhenNamespaceNameIsEmpty()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
 
         string namespaceName = "";
         var password = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, "password", special: false);
@@ -199,7 +241,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerDatabaseResourceShouldThrowWhenDatabaseNameIsNull()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
 
         string name = "surreal";
         string namespaceName = "ns";
@@ -216,7 +258,7 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerDatabaseResourceShouldThrowWhenDatabaseNameIsEmpty()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
+        var distributedApplicationBuilder = TestDistributedApplicationBuilder.Create();
 
         string name = "surreal";
         string namespaceName = "ns";
@@ -233,15 +275,27 @@ public class SurrealDbPublicApiTests
     [Fact]
     public void CtorSurrealServerDatabaseResourceShouldThrowWhenParentIsNull()
     {
-        var distributedApplicationBuilder = DistributedApplication.CreateBuilder([]);
-
         string name = "surreal";
         string databaseName = "db1";
-        var password = ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(distributedApplicationBuilder, "password", special: false);
         SurrealDbNamespaceResource parent = null!;
         var action = () => new SurrealDbDatabaseResource(name, databaseName, parent);
 
         var exception = Assert.Throws<ArgumentNullException>(action);
         Assert.Equal(nameof(parent), exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void CtorSurrealistContainerResourceShouldThrowWhenNameIsNullOrEmpty(bool isNull)
+    {
+        var name = isNull ? null! : string.Empty;
+
+        var action = () => new SurrealistContainerResource(name);
+
+        var exception = isNull
+            ? Assert.Throws<ArgumentNullException>(action)
+            : Assert.Throws<ArgumentException>(action);
+        Assert.Equal(nameof(name), exception.ParamName);
     }
 }
