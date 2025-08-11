ef21d6b016d2ba34ffdca7683de53b9ac2340653
Martin Thwaites <github@my2cents.co.uk>
2025-08-11 21:09:19 +0000

    Renames

---
+++ src/CommunityToolkit.Aspire.Hosting.OpenTelemetryCollector/OpenTelemetryCollectorResource.cs
@@ -0,0 +1,23 @@
+﻿using Aspire.Hosting.ApplicationModel;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// The collector resource
+/// </summary>
+/// <param name="name">Name of the resource</param>
+public class OpenTelemetryCollectorResource(string name) : ContainerResource(name)
+{
+    internal static string GrpcEndpointName = "grpc";
+    internal static string HttpEndpointName = "http";
+
+    /// <summary>
+    /// gRPC Endpoint
+    /// </summary>
+    public EndpointReference GrpcEndpoint => new(this, GrpcEndpointName);
+
+    /// <summary>
+    /// HTTP Endpoint
+    /// </summary>
+    public EndpointReference HttpEndpoint => new(this, HttpEndpointName);
+}
