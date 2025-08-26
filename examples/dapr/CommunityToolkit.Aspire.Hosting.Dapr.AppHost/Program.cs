7e3e2185628d3931af7df8323c93932d9b7967a3
Elvis Nieves Miranda <elvis.nievesmiranda@gmail.com>
2025-08-26 01:32:34 -0400

    chore: make configuration example cleaner

--- examples/dapr/CommunityToolkit.Aspire.Hosting.Dapr.AppHost/Program.cs
+++ examples/dapr/CommunityToolkit.Aspire.Hosting.Dapr.AppHost/Program.cs
@@ -5,11 +5,17 @@ var redis = builder.AddRedis("redis").WithRedisInsight();
 
 var stateStore = builder.AddDaprStateStore("statestore");
 
-var pubSub = builder.AddDaprPubSub("pubsub")
-                    .WithMetadata("redisHost",
-                        ReferenceExpression.Create($"{redis.Resource.PrimaryEndpoint.Property(EndpointProperty.Host)}:{redis.Resource.PrimaryEndpoint.Property(EndpointProperty.TargetPort)}")
-                    )
-                    .WaitFor(redis);
+var redisHost= redis.Resource.PrimaryEndpoint.Property(EndpointProperty.Host);
+var redisTargetPort = redis.Resource.PrimaryEndpoint.Property(EndpointProperty.TargetPort);
+
+var pubSub = builder
+  .AddDaprPubSub("pubsub")
+  .WithMetadata(
+    "redisHost",
+    ReferenceExpression.Create(
+      $"{redisHost}:{redisTargetPort}"
+    )
+  );
 
 builder.AddProject<Projects.CommunityToolkit_Aspire_Hosting_Dapr_ServiceA>("servicea")
        .WithReference(stateStore)
