fe25c66ad47fa96ed31a598b1fadcc06e1635926
Elvis Nieves Miranda <elvis.nievesmiranda@gmail.com>
2025-08-26 01:50:01 -0400

    chore: remove unused using

--- tests/dapr-shared/AddDaprPubSubTests.cs
+++ tests/dapr-shared/AddDaprPubSubTests.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Hosting;
-using Aspire.Hosting.ApplicationModel;
 
 namespace CommunityToolkit.Aspire.Hosting.Dapr.Tests;
 
