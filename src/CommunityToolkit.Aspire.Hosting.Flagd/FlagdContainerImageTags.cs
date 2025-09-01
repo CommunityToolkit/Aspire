1e66a9917429c6ce01ca52964c3ec358ee88f037
André Silva <2493377+askpt@users.noreply.github.com>
2025-09-01 17:16:43 +0100

    Update Flagd container image tag to v0.12.9
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/CommunityToolkit.Aspire.Hosting.Flagd/FlagdContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.Flagd/FlagdContainerImageTags.cs
@@ -6,6 +6,6 @@ internal static class FlagdContainerImageTags
     public const string Registry = "ghcr.io";
     /// <summary>open-feature/flagd</summary>
     public const string Image = "open-feature/flagd";
-    /// <summary>v0.11.6</summary>
-    public const string Tag = "v0.11.6";
+    /// <summary>v0.12.9</summary>
+    public const string Tag = "v0.12.9";
 }
