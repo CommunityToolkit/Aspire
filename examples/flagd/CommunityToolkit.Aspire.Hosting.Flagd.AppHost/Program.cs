e97bca35808c9b4a671227fec8f9400e4cd3bfc8
André Silva <2493377+askpt@users.noreply.github.com>
2025-09-02 07:33:11 +0100

    Add OpenFeature Flagd provider and configuration flags
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- examples/flagd/CommunityToolkit.Aspire.Hosting.Flagd.AppHost/Program.cs
+++ examples/flagd/CommunityToolkit.Aspire.Hosting.Flagd.AppHost/Program.cs
@@ -1,7 +1,20 @@
+using OpenFeature.Contrib.Providers.Flagd;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Add flagd with local flag configuration file
-var flagd = builder.AddFlagd("flagd", "flags.json")
+var flagd = builder.AddFlagd("flagd", "./flags")
     .WithLogging("debug");
+
+builder.Eventing.Subscribe<AfterResourcesCreatedEvent>(static async (@event, cancellationToken) =>
+    {
+        // When the resources are created, set the OpenFeature provider to use Flagd (debug purposes)
+        await OpenFeature.Api.Instance.SetProviderAsync(new FlagdProvider());
+
+        var flagClient = OpenFeature.Api.Instance.GetClient();
+        var welcomeBanner = await flagClient.GetBooleanDetailsAsync("welcome-banner", false);
+        var backgroundColor = await flagClient.GetStringDetailsAsync("background-color", "000000");
+        var apiVersion = await flagClient.GetStringDetailsAsync("api-version", "0.1");
+    });
 
 builder.Build().Run();
