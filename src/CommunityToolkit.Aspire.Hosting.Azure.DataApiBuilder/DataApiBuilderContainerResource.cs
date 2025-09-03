7e28aa1a96f9fa8826d0f6f70260d361aff63cab
Tommaso Stocchi <tstocchi@microsoft.com>
2025-09-03 16:52:16 +0200

    adds runasexecutable api for dab

--- src/CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder/DataApiBuilderContainerResource.cs
+++ src/CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder/DataApiBuilderContainerResource.cs
@@ -3,11 +3,34 @@
 /// <summary>
 /// A resource that represents Data Api Builder.
 /// </summary>
+public interface IDataApiBuilderResource : IResourceWithEnvironment, IResourceWithServiceDiscovery, IResourceWithWaitSupport { }
+
+/// <summary>
+/// A resource that represents Data Api Builder.
+/// </summary>
 /// <param name="name">The name of the resource.</param>
 /// <param name="entrypoint">An optional container entrypoint.</param>
 
 public class DataApiBuilderContainerResource(string name, string? entrypoint = null)
-    : ContainerResource(name, entrypoint), IResourceWithServiceDiscovery
+    : ContainerResource(name, entrypoint), IDataApiBuilderResource
+{
+    internal const string HttpEndpointName = "http";
+    internal const string HttpsEndpointName = "https";
+
+    internal const int HttpEndpointPort = 5000;
+    internal const int HttpsEndpointPort = 5001;
+
+    internal int? HttpPort;
+
+    internal string[]? ConfigFilePaths { get; set; }
+}
+
+/// <summary>
+/// A resource that represents Data Api Builder.
+/// </summary>
+/// <param name="name">The name of the resource.</param>
+public class DataApiBuilderExecutableResource(string name)
+    : ExecutableResource(name, "dab", "./"), IDataApiBuilderResource
 {
     internal const string HttpEndpointName = "http";
     internal const string HttpsEndpointName = "https";
--- src/CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder/DataApiBuilderHostingExtension.cs
+++ src/CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder/DataApiBuilderHostingExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Aspire.Hosting.ApplicationModel;
 using CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder;
+using YamlDotNet.Serialization;
 
 namespace Aspire.Hosting;
 
@@ -18,7 +19,7 @@ public static class DataApiBuilderHostingExtension
     /// At this time, this .NET Aspire DAB integration only supports HTTPS ports. 
     /// You can <see href="https://learn.microsoft.com/en-us/aspnet/core/security/docker-https?view=aspnetcore-8.0#running-pre-built-container-images-with-https">deploy DAB with HTTPS and custom certs</see> in production.
     /// </remarks>
-    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <returns>A reference to the <see cref="IResourceBuilder{DataApiBuilderContainerResource}"/>.</returns>
     public static IResourceBuilder<DataApiBuilderContainerResource> AddDataAPIBuilder(this IDistributedApplicationBuilder builder,
         [ResourceName] string name,
         params string[] configFilePaths)
@@ -37,7 +38,7 @@ public static class DataApiBuilderHostingExtension
     /// At this time, this .NET Aspire DAB integration only supports HTTPS ports. 
     /// You can <see href="https://learn.microsoft.com/en-us/aspnet/core/security/docker-https?view=aspnetcore-8.0#running-pre-built-container-images-with-https">deploy DAB with HTTPS and custom certs</see> in production.
     /// </remarks>
-    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <returns>A reference to the <see cref="IResourceBuilder{DataApiBuilderContainerResource}"/>.</returns>
     public static IResourceBuilder<DataApiBuilderContainerResource> AddDataAPIBuilder(this IDistributedApplicationBuilder builder,
         [ResourceName] string name,
         int? httpPort = null,
@@ -62,6 +63,10 @@ public static class DataApiBuilderHostingExtension
             configFilePaths = ["./dab-config.json"];
         }
 
+        // Store the config file paths on the resource so RunAsExecutable can access them.
+        resource.HttpPort = httpPort;
+        resource.ConfigFilePaths = configFilePaths;
+
         foreach (var configFilePath in configFilePaths)
         {
             var configFileName = File.Exists(configFilePath)
@@ -73,8 +78,70 @@ public static class DataApiBuilderHostingExtension
 
         return rb;
     }
+    
+    /// <summary>
+    /// Configure the resource builder to run the Data API Builder as an executable in Development mode.
+    /// </summary>
+    /// <param name="builder">The <see cref="IResourceBuilder{DataApiBuilderContainerResource}"/> to run as executable.</param>
+    /// <param name="configure">The <see cref="IResourceBuilder{DataApiBuilderExecutableResource}"/> to run as executable.</param>
+    /// <remarks>
+    /// If publish mode is selected, this will be ignored.
+    /// DAB cli is required to use this option.
+    /// You can look at the documentation to <see href="https://learn.microsoft.com/en-us/azure/data-api-builder/how-to/install-cli">install dab cli</see>.
+    /// </remarks>
+    /// <returns>A reference to the <see cref="IResourceBuilder{DataApiBuilderExecutableResource}"/>.</returns>
+    public static IResourceBuilder<IDataApiBuilderResource> RunAsExecutable(this IResourceBuilder<DataApiBuilderContainerResource> builder,
+        Action<IResourceBuilder<DataApiBuilderExecutableResource>>? configure = null)
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsPublishMode)
+        {
+            return builder;
+        }
 
-    private static IResourceBuilder<DataApiBuilderContainerResource> WithDataApiBuilderDefaults(
-        this IResourceBuilder<DataApiBuilderContainerResource> builder) =>
+        // I'm using the same strategy used for PublishAsDockerfile in Aspire
+        builder.ApplicationBuilder.Resources.Remove(builder.Resource);
+
+        // Configure the resource builder to run the Data API Builder as an executable
+        var resource = new DataApiBuilderExecutableResource(builder.Resource.Name);
+
+        // We build the list of arguments to pass to the dab command
+        // The final command should be similar to dab start --config file1.json file2.json
+        var allArgs = new List<string> { "start" };
+
+        // The config file paths should have been set by AddDataAPIBuilder. Enforce invocation order.
+        var configFilePaths = builder.Resource.ConfigFilePaths;
+        if (configFilePaths is null || configFilePaths.Length == 0)
+        {
+            throw new InvalidOperationException("RunAsExecutable must be called after AddDataAPIBuilder so the config file paths are available.");
+        }
+
+        allArgs.Add("--config");
+
+        foreach (var configFilePath in configFilePaths)
+        {
+            var configFileName = File.Exists(configFilePath)
+                ? Path.GetFileName(configFilePath)
+                : throw new FileNotFoundException($"Config file not found: {configFilePath}");
+
+            allArgs.Add(configFileName);
+        }
+
+        var rb = builder.ApplicationBuilder.AddResource(resource)
+            .WithHttpEndpoint(port: builder.Resource.HttpPort,
+                targetPort: DataApiBuilderContainerResource.HttpEndpointPort,
+                name: DataApiBuilderContainerResource.HttpEndpointName)
+            .WithHttpsEndpoint(targetPort: DataApiBuilderContainerResource.HttpsEndpointPort,
+                name: DataApiBuilderContainerResource.HttpsEndpointName)
+            .WithEnvironment("ASPNETCORE_URLS", "http://localhost:5000;https://localhost:5001")
+            .WithDataApiBuilderDefaults()
+            .WithArgs([.. allArgs]);
+
+        configure?.Invoke(rb);
+
+        return rb;
+    }
+
+    private static IResourceBuilder<T> WithDataApiBuilderDefaults<T>(
+        this IResourceBuilder<T> builder) where T : IDataApiBuilderResource =>
         builder.WithOtlpExporter();
 }
