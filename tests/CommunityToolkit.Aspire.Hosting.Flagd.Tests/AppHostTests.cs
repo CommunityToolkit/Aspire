537feb903e6c174454dd17441b87f77cfed1f4d0
André Silva <2493377+askpt@users.noreply.github.com>
2025-09-15 16:24:55 +0000

    Add tests for flagd provider and assert expected values
    Signed-off-by: GitHub <noreply@github.com>

--- tests/CommunityToolkit.Aspire.Hosting.Flagd.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Flagd.Tests/AppHostTests.cs
@@ -1,5 +1,6 @@
 using Aspire.Components.Common.Tests;
 using CommunityToolkit.Aspire.Testing;
+using OpenFeature.Contrib.Providers.Flagd;
 
 namespace CommunityToolkit.Aspire.Hosting.Flagd.Tests;
 
@@ -11,6 +12,19 @@ public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit
     {
         var resourceName = "flagd";
         await fixture.ResourceNotificationService.WaitForResourceAsync(resourceName).WaitAsync(TimeSpan.FromMinutes(1));
-        var httpClient = fixture.CreateHttpClient(resourceName);
+
+        var connectionString = await fixture.GetConnectionString(resourceName);
+        Assert.NotNull(connectionString);
+
+        await OpenFeature.Api.Instance.SetProviderAsync(new FlagdProvider(new Uri(connectionString)));
+
+        var flagClient = OpenFeature.Api.Instance.GetClient();
+        var welcomeBanner = await flagClient.GetBooleanDetailsAsync("welcome-banner", false);
+        var backgroundColor = await flagClient.GetStringDetailsAsync("background-color", "000000");
+        var apiVersion = await flagClient.GetStringDetailsAsync("api-version", "0.1");
+
+        Assert.False(welcomeBanner.Value);
+        Assert.Equal("#FF0000", backgroundColor.Value);
+        Assert.Equal("1.0", apiVersion.Value);
     }
 }
