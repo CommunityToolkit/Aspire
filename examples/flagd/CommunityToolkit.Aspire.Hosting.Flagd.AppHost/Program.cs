6e80257fc83beaada19970767518985e983b92d6
André Silva <2493377+askpt@users.noreply.github.com>
2025-10-01 18:28:11 +0000

    Add support for OpenFeature.Providers.Ofrep and update related configurations
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- examples/flagd/CommunityToolkit.Aspire.Hosting.Flagd.AppHost/Program.cs
+++ examples/flagd/CommunityToolkit.Aspire.Hosting.Flagd.AppHost/Program.cs
@@ -4,19 +4,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add flagd with local flag configuration file
 var flagd = builder
-    .AddFlagd("flagd", 8013)
-    .WithBindFileSync("./flags/", "flagd.json")
+    .AddFlagd("flagd")
+    .WithBindFileSync("./flags/")
     .WithLogging();
-
-builder.Eventing.Subscribe<AfterResourcesCreatedEvent>(static async (@event, cancellationToken) =>
-    {
-        // When the resources are created, set the OpenFeature provider to use Flagd (debug purposes)
-        await OpenFeature.Api.Instance.SetProviderAsync(new FlagdProvider());
-
-        var flagClient = OpenFeature.Api.Instance.GetClient();
-        var welcomeBanner = await flagClient.GetBooleanDetailsAsync("welcome-banner", false);
-        var backgroundColor = await flagClient.GetStringDetailsAsync("background-color", "000000");
-        var apiVersion = await flagClient.GetStringDetailsAsync("api-version", "0.1");
-    });
 
 builder.Build().Run();
--- tests/CommunityToolkit.Aspire.Hosting.Flagd.Tests/AppHostTests.cs
+++ tests/CommunityToolkit.Aspire.Hosting.Flagd.Tests/AppHostTests.cs
@@ -1,6 +1,8 @@
 using Aspire.Components.Common.Tests;
 using CommunityToolkit.Aspire.Testing;
 using OpenFeature.Contrib.Providers.Flagd;
+using OpenFeature.Providers.Ofrep;
+using OpenFeature.Providers.Ofrep.Configuration;
 
 namespace CommunityToolkit.Aspire.Hosting.Flagd.Tests;
 
@@ -8,7 +10,7 @@ namespace CommunityToolkit.Aspire.Hosting.Flagd.Tests;
 public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Flagd_AppHost> fixture) : IClassFixture<AspireIntegrationTestFixture<Projects.CommunityToolkit_Aspire_Hosting_Flagd_AppHost>>
 {
     [Fact]
-    public async Task ResourceStartsAndRespondsCorrectly()
+    public async Task ResourceStartsAndRespondsCorrectlyForFlagdEvaluation()
     {
         var resourceName = "flagd";
         await fixture.ResourceNotificationService.WaitForResourceHealthyAsync(resourceName).WaitAsync(TimeSpan.FromMinutes(1));
@@ -23,7 +25,31 @@ public class AppHostTests(AspireIntegrationTestFixture<Projects.CommunityToolkit
         var backgroundColor = await flagClient.GetStringDetailsAsync("background-color", "000000");
         var apiVersion = await flagClient.GetStringDetailsAsync("api-version", "0.1");
 
-        Assert.False(welcomeBanner.Value);
+        Assert.True(welcomeBanner.Value);
+        Assert.Equal("#FF0000", backgroundColor.Value);
+        Assert.Equal("1.0", apiVersion.Value);
+    }
+
+    [Fact(Skip = "This test is failing because the Ofrep endpoint is not mapped correctly")]
+    public async Task ResourceStartsAndRespondsCorrectlyForOfrepEvaluation()
+    {
+        var resourceName = "flagd";
+        await fixture.ResourceNotificationService.WaitForResourceHealthyAsync(resourceName).WaitAsync(TimeSpan.FromMinutes(1));
+
+        var connectionString = await fixture.GetConnectionString(resourceName);
+        Assert.NotNull(connectionString);
+
+        // Configure the provider
+        var config = new OfrepOptions(connectionString);
+
+        await OpenFeature.Api.Instance.SetProviderAsync(new OfrepProvider(config));
+
+        var flagClient = OpenFeature.Api.Instance.GetClient();
+        var welcomeBanner = await flagClient.GetBooleanDetailsAsync("welcome-banner", false);
+        var backgroundColor = await flagClient.GetStringDetailsAsync("background-color", "000000");
+        var apiVersion = await flagClient.GetStringDetailsAsync("api-version", "0.1");
+
+        Assert.True(welcomeBanner.Value);
         Assert.Equal("#FF0000", backgroundColor.Value);
         Assert.Equal("1.0", apiVersion.Value);
     }
