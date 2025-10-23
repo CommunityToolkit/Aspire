f0f286f415125282fbaa4c1dd72a1e04488a97b3
Erik Ejlskov Jensen <eej@delegate.dk>
2025-10-23 14:38:24 +0200

    Attempt to fix issue #913

--- src/CommunityToolkit.Aspire.Hosting.SqlDatabaseProjects/SqlProjectBuilderExtensions.cs
+++ src/CommunityToolkit.Aspire.Hosting.SqlDatabaseProjects/SqlProjectBuilderExtensions.cs
@@ -20,6 +20,7 @@ public static class SqlProjectBuilderExtensions
         if (!MSBuildLocator.IsRegistered)
         {
             MSBuildLocator.RegisterDefaults();
+            MSBuildLocator.AllowQueryAllDotnetLocations = true;
         }
     }
 
