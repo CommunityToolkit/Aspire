1ac2ad3c7ebc37c01868f10f9e9a946ed462afe9
Aaron Powell <me@aaron-powell.com>
2025-11-10 05:19:03 +0000

    Disabling JSON schema tests as we don't generate those files

--- tests/CommunityToolkit.Aspire.Testing/ConformanceTests.cs
+++ tests/CommunityToolkit.Aspire.Testing/ConformanceTests.cs
@@ -335,7 +335,7 @@ public abstract class ConformanceTests<TService, TOptions>
         Assert.Contains(healthReport.Entries, entry => entry.Value.Status == expected);
     }
 
-    [Fact]
+    [Fact(Skip = "https://github.com/CommunityToolkit/Aspire/issues/112")]
     public void ConfigurationSchemaValidJsonConfigTest()
     {
         var schema = JsonSchema.FromFile(JsonSchemaPath);
@@ -346,7 +346,7 @@ public abstract class ConformanceTests<TService, TOptions>
         Assert.True(results.IsValid);
     }
 
-    [Fact]
+    [Fact(Skip = "https://github.com/CommunityToolkit/Aspire/issues/112")]
     public void ConfigurationSchemaInvalidJsonConfigTest()
     {
         var schema = JsonSchema.FromFile(JsonSchemaPath);
