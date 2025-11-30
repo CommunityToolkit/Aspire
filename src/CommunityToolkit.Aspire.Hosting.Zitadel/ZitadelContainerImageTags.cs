5b328a0fe7d1dbea622dbe51034f6d80433ee774
Wannes Gennar <wannes.gennar@gmail.com>
2025-11-30 12:02:59 +0100

    feat: add Zitadel host integration

---
+++ src/CommunityToolkit.Aspire.Hosting.Zitadel/ZitadelContainerImageTags.cs
@@ -0,0 +1,13 @@
+namespace CommunityToolkit.Aspire.Hosting.Zitadel;
+
+internal static class ZitadelContainerImageTags
+{
+    /// <remarks>Github Container Registry</remarks>
+    public const string Registry = "ghcr.io";
+
+    /// <remarks>zitadel/zitadel</remarks>
+    public const string Image = "zitadel/zitadel";
+
+    /// <remarks>v4.7.0</remarks>
+    public const string Tag = "v4.7.0";
+}
