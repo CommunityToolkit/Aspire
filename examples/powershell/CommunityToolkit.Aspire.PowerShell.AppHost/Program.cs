91576a2d5e8e3bbedd408a66305b295763619f5b
Aaron Powell <me@aaron-powell.com>
2025-12-09 05:28:55 +0000

    The path when the scripts run during tests is different to just running the app host

--- examples/powershell/CommunityToolkit.Aspire.PowerShell.AppHost/Program.cs
+++ examples/powershell/CommunityToolkit.Aspire.PowerShell.AppHost/Program.cs
@@ -14,6 +14,7 @@ var script1 = ps.AddScript("script1", """
     param($name)
 
     write-information "Hello, $name"
+    write-information $pwd
 
     # uncommenting this will hang the script if you don't attach the pwsh debugger
     # wait-debugger
@@ -22,15 +23,19 @@ var script1 = ps.AddScript("script1", """
 
     # only run this if Azure CLI is installed
     if ((gcm az -ErrorAction SilentlyContinue) -ne $null) {
+        # When in the tests the PWD is wrong so we should test for it and fix it
+        if ($pwd -like "*CommunityToolkit.Aspire.Hosting.PowerShell.Tests*") {
+            write-information "Fixing PWD from $pwd"
+            Set-Location (Join-Path $pwd "../../../../../examples/powershell/CommunityToolkit.Aspire.PowerShell.AppHost")
+            write-information "New PWD is $pwd"
+        }
 
         az storage container create --connection-string $myblob -n demo
-        az storage blob upload --connection-string $myblob -c demo --file ./scripts/script.ps1
+        az storage blob upload --connection-string $myblob -c demo --file ./Scripts/script.ps1
         write-information "Blob uploaded"
 
     } else {
-
         write-warning "Azure CLI not found, skipping blob upload"
-
     }
     write-information $pwd
 
@@ -39,6 +44,14 @@ var script1 = ps.AddScript("script1", """
 
 // outputs "the sum of 2 and 3 is 5"
 var script2 = ps.AddScript("script2", """
+    write-information 'Getting there...'
+    if ($pwd -like "*CommunityToolkit.Aspire.Hosting.PowerShell.Tests*") {
+        write-information "Fixing PWD from $pwd"
+        Set-Location (Join-Path $pwd "../../../../../examples/powershell/CommunityToolkit.Aspire.PowerShell.AppHost")
+        write-information "New PWD is $pwd"
+    }
+    write-information $PWD
+
     & ./Scripts/script.ps1 @args
     """)
     .WithArgs(2, 3)
