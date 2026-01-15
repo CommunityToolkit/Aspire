2fa13d64be17651a7d998341016d1d00a2a93d2b
Wannes Gennar <wannes.gennar@gmail.com>
2026-01-15 08:08:02 +0100

    feat: add watch support for Permify

--- examples/permify/CommunityToolkit.Aspire.Hosting.Permify.AppHost/AppHost.cs
+++ examples/permify/CommunityToolkit.Aspire.Hosting.Permify.AppHost/AppHost.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddPermify("permify");
+var database = builder.AddPostgres("permify-postgres")
+    .AddDatabase("permify-db");
+
+builder.AddPermify("permify")
+    .WithWatchSupport(database);
 
 builder.Build().Run();
