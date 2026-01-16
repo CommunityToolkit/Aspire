bf00427d87f57707a6ab628ec81079d8cfdc71eb
Alirexaa <70141416+Alirexaa@users.noreply.github.com>
2026-01-16 16:14:35 +0000

    [create-pull-request] automated change

--- src/CommunityToolkit.Aspire.Hosting.Golang/api/CommunityToolkit.Aspire.Hosting.Golang.cs
+++ src/CommunityToolkit.Aspire.Hosting.Golang/api/CommunityToolkit.Aspire.Hosting.Golang.cs
@@ -16,6 +16,10 @@ namespace Aspire.Hosting
 
         [System.Obsolete("Use AddGolangApp with buildTags parameter instead. This method will be removed in a future version.")]
         public static ApplicationModel.IResourceBuilder<ApplicationModel.GolangAppExecutableResource> AddGolangApp(this IDistributedApplicationBuilder builder, string name, string workingDirectory, string[] args) { throw null; }
+
+        public static ApplicationModel.IResourceBuilder<ApplicationModel.GolangAppExecutableResource> WithGoModDownload(this ApplicationModel.IResourceBuilder<ApplicationModel.GolangAppExecutableResource> builder, bool install = true, System.Action<ApplicationModel.IResourceBuilder<ApplicationModel.GoModInstallerResource>>? configureInstaller = null) { throw null; }
+
+        public static ApplicationModel.IResourceBuilder<ApplicationModel.GolangAppExecutableResource> WithGoModTidy(this ApplicationModel.IResourceBuilder<ApplicationModel.GolangAppExecutableResource> builder, bool install = true, System.Action<ApplicationModel.IResourceBuilder<ApplicationModel.GoModInstallerResource>>? configureInstaller = null) { throw null; }
     }
 }
 
@@ -24,5 +28,10 @@ namespace Aspire.Hosting.ApplicationModel
     public partial class GolangAppExecutableResource : ExecutableResource, IResourceWithServiceDiscovery, IResourceWithEndpoints, IResource
     {
         public GolangAppExecutableResource(string name, string workingDirectory) : base(default!, default!, default!) { }
+    }
+
+    public partial class GoModInstallerResource : ExecutableResource
+    {
+        public GoModInstallerResource(string name, string workingDirectory) : base(default!, default!, default!) { }
     }
 }
