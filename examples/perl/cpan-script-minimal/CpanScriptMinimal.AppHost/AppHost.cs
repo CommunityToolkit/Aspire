f63c58d02b1583e1455ace653f3c551865ccecbd
Omnideth <omnideth@gmail.com>
2026-03-20 18:37:34 -0500

    This is _hopefully_ the final commit.
    Examples cleaned up, condensed to one minimal example to show two perl things acting together with aspire as the glue.

--- examples/perl/cpan-script-minimal/CpanScriptMinimal.AppHost/AppHost.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddPerlScript("cpan-worker", "../scripts", "Worker.pl")
-    .WithPackage("OpenTelemetry::SDK", skipTest: true, force: true);
-    //.WithLocalLib("local"); //would automatically swap to Cpanm, may fix at a later date, documentation is inconclusive for how to wire together.
-
-builder.Build().Run();
--- examples/perl/cpanm-api-integration/CpanmApiIntegration.AppHost/AppHost.cs
+++ examples/perl/cpanm-api-integration/CpanmApiIntegration.AppHost/AppHost.cs
@@ -1,9 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddPerlApi("perl-api", ".", "../scripts/API.pl")
+var perlApi = builder.AddPerlApi("perl-api", ".", "../scripts/API.pl")
     .WithCpanMinus()
     .WithPackage("Mojolicious::Lite", force: true, skipTest: true)
     .WithLocalLib("local") //to avoid 'sudo' applying to system perl.   
     .WithHttpEndpoint(name: "http", env: "PORT");
+
+builder.AddPerlScript("perl-driver", "../scripts", "driver.pl")
+    .WithEnvironment("API_URL", perlApi.GetEndpoint("http"))
+    .WithReference(perlApi)
+    .WaitFor(perlApi);
 
 builder.Build().Run();
