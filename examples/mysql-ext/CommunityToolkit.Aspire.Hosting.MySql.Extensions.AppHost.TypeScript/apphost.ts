15dffb9a86c1e805882d9547605ff3aa71e54f99
Aaron Powell <me@aaron-powell.com>
2026-05-12 16:54:15 +1000

    Fixing API usage in mysql apphost.ts

--- examples/mysql-ext/CommunityToolkit.Aspire.Hosting.MySql.Extensions.AppHost.TypeScript/apphost.ts
+++ examples/mysql-ext/CommunityToolkit.Aspire.Hosting.MySql.Extensions.AppHost.TypeScript/apphost.ts
@@ -1,23 +1,22 @@
-import { createBuilder } from './.modules/aspire.js';
+import { createBuilder } from "./.modules/aspire.js";
 
 const builder = await createBuilder();
 
-const mysql = await builder.addMySql('mysql');
+const mysql = await builder.addMySql("mysql");
 
 await mysql
     .withAdminer({
-        configureContainer: async (_adminer) => { },
-        containerName: 'mysql-adminer'
+        configureContainer: async (_adminer) => {},
+        containerName: "mysql-adminer",
     })
     .withDbGate({
-        configureContainer: async (_dbGate) => { },
-        containerName: 'mysql-dbgate'
+        configureContainer: async (_dbGate) => {},
+        containerName: "mysql-dbgate",
     });
 
-const resolvedMySql = await mysql;
-const _primaryEndpoint = await resolvedMySql.getEndpoint("tcp");
-const _host = await _primaryEndpoint.host.get();
-const _port = await _primaryEndpoint.port.get();
-const _connectionString = await resolvedMySql.connectionStringExpression.get();
+const _primaryEndpoint = await mysql.getEndpoint("tcp");
+const _host = await _primaryEndpoint.host();
+const _port = await _primaryEndpoint.port();
+const _connectionString = await mysql.connectionStringExpression();
 
 await builder.build().run();
