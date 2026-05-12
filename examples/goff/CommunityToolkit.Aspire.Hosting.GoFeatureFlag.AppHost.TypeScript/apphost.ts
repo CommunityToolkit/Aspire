676499b0e382890fe3976cf49938e331452daf28
Aaron Powell <me@aaron-powell.com>
2026-05-12 16:31:00 +1000

    Fixing API usage in goff apphost.ts

--- examples/goff/CommunityToolkit.Aspire.Hosting.GoFeatureFlag.AppHost.TypeScript/apphost.ts
+++ examples/goff/CommunityToolkit.Aspire.Hosting.GoFeatureFlag.AppHost.TypeScript/apphost.ts
@@ -1,10 +1,10 @@
-import { createBuilder, GoFeatureFlagLogLevel } from './.modules/aspire.js';
+import { createBuilder, GoFeatureFlagLogLevel } from "./.modules/aspire.js";
 
 const builder = await createBuilder();
 
 const goff = await builder.addGoFeatureFlag("goff", {
     pathToConfigFile: "/goff/goff-proxy.yaml",
-    port: 11031
+    port: 11031,
 });
 
 await goff.withDataVolume({ name: "goff-data" });
@@ -14,18 +14,16 @@ await goff.withLogLevel(GoFeatureFlagLogLevel.Debug);
 const goff2 = await builder.addGoFeatureFlag("goff2");
 await goff2.withGoffBindMount("./goff");
 
-const goffResource = await goff;
-const _primaryEndpoint = await goffResource.getEndpoint("http");
-const _host = await _primaryEndpoint.host.get();
-const _port = await _primaryEndpoint.port.get();
-const _uri = await _primaryEndpoint.url.get();
-const _connectionString = await goffResource.connectionStringExpression.get();
+const _primaryEndpoint = await goff.getEndpoint("http");
+const _host = await _primaryEndpoint.host();
+const _port = await _primaryEndpoint.port();
+const _uri = await _primaryEndpoint.url();
+const _connectionString = await goff.connectionStringExpression();
 
-const goff2Resource = await goff2;
-const _primaryEndpoint2 = await goff2Resource.getEndpoint("http");
-const _host2 = await _primaryEndpoint2.host.get();
-const _port2 = await _primaryEndpoint2.port.get();
-const _uri2 = await _primaryEndpoint2.url.get();
-const _connectionString2 = await goff2Resource.connectionStringExpression.get();
+const _primaryEndpoint2 = await goff2.getEndpoint("http");
+const _host2 = await _primaryEndpoint2.host();
+const _port2 = await _primaryEndpoint2.port();
+const _uri2 = await _primaryEndpoint2.url();
+const _connectionString2 = await goff2.connectionStringExpression();
 
 await builder.build().run();
