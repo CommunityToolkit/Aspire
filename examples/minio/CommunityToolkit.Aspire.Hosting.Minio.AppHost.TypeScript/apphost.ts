9a2b682b6768f2fb22815606788b26d309aaa7f2
Aaron Powell <me@aaron-powell.com>
2026-05-12 16:52:55 +1000

    Fixing API usage in MinIO apphost.ts

--- examples/minio/CommunityToolkit.Aspire.Hosting.Minio.AppHost.TypeScript/apphost.ts
+++ examples/minio/CommunityToolkit.Aspire.Hosting.Minio.AppHost.TypeScript/apphost.ts
@@ -1,22 +1,36 @@
-import { createBuilder } from './.modules/aspire.js';
+import { createBuilder } from "./.modules/aspire.js";
 
 const builder = await createBuilder();
 
-const rootUser = await builder.addParameterWithValue("minio-root-user", "minioadmin");
-const rootPassword = await builder.addParameterWithValue("minio-root-password", "minio-password", { secret: true });
-const overrideUser = await builder.addParameterWithValue("override-root-user", "override-admin");
-const overridePassword = await builder.addParameterWithValue("override-root-password", "override-password", { secret: true });
+const rootUser = await builder.addParameterWithValue(
+    "minio-root-user",
+    "minioadmin",
+);
+const rootPassword = await builder.addParameterWithValue(
+    "minio-root-password",
+    "minio-password",
+    { secret: true },
+);
+const overrideUser = await builder.addParameterWithValue(
+    "override-root-user",
+    "override-admin",
+);
+const overridePassword = await builder.addParameterWithValue(
+    "override-root-password",
+    "override-password",
+    { secret: true },
+);
 
 const minio = await builder.addMinioContainer("minio", {
     rootUser,
     rootPassword,
-    port: 9000
+    port: 9000,
 });
 
 const minioDefaults = await builder.addMinioContainer("minio-defaults");
 const minioBindMount = await builder.addMinioContainer("minio-bind-mount", {
     rootUser: overrideUser,
-    rootPassword: overridePassword
+    rootPassword: overridePassword,
 });
 
 await minio.withHostPort({ port: 9100 }).withDataVolume({ name: "minio-data" });
@@ -24,27 +38,26 @@ await minioDefaults.withUserName(overrideUser).withPassword(overridePassword);
 await minioDefaults.withDataVolume();
 await minioBindMount.withDataBindMount("./runtime-data/minio-bind");
 
-const minioResource = await minio;
-const _minioRootUser = await minioResource.rootUser.get();
-const _minioPasswordParameter = await minioResource.passwordParameter.get();
-const _minioPrimaryEndpoint = await minioResource.getEndpoint("http");
-const _minioHost = await _minioPrimaryEndpoint.host.get();
-const _minioPort = await _minioPrimaryEndpoint.port.get();
-const _minioUri = await _minioPrimaryEndpoint.url.get();
-const _minioConnectionString = await minioResource.connectionStringExpression.get();
+const _minioRootUser = await minio.rootUser();
+const _minioPasswordParameter = await minio.passwordParameter();
+const _minioPrimaryEndpoint = await minio.getEndpoint("http");
+const _minioHost = await _minioPrimaryEndpoint.host();
+const _minioPort = await _minioPrimaryEndpoint.port();
+const _minioUri = await _minioPrimaryEndpoint.url();
+const _minioConnectionString = await minio.connectionStringExpression();
 
-const minioDefaultsResource = await minioDefaults;
-const _defaultsPrimaryEndpoint = await minioDefaultsResource.getEndpoint("http");
-const _defaultsHost = await _defaultsPrimaryEndpoint.host.get();
-const _defaultsPort = await _defaultsPrimaryEndpoint.port.get();
-const _defaultsUri = await _defaultsPrimaryEndpoint.url.get();
-const _defaultsConnectionString = await minioDefaultsResource.connectionStringExpression.get();
+const _defaultsPrimaryEndpoint = await minioDefaults.getEndpoint("http");
+const _defaultsHost = await _defaultsPrimaryEndpoint.host();
+const _defaultsPort = await _defaultsPrimaryEndpoint.port();
+const _defaultsUri = await _defaultsPrimaryEndpoint.url();
+const _defaultsConnectionString =
+    await minioDefaults.connectionStringExpression();
 
-const minioBindMountResource = await minioBindMount;
-const _bindMountPrimaryEndpoint = await minioBindMountResource.getEndpoint("http");
-const _bindMountHost = await _bindMountPrimaryEndpoint.host.get();
-const _bindMountPort = await _bindMountPrimaryEndpoint.port.get();
-const _bindMountUri = await _bindMountPrimaryEndpoint.url.get();
-const _bindMountConnectionString = await minioBindMountResource.connectionStringExpression.get();
+const _bindMountPrimaryEndpoint = await minioBindMount.getEndpoint("http");
+const _bindMountHost = await _bindMountPrimaryEndpoint.host();
+const _bindMountPort = await _bindMountPrimaryEndpoint.port();
+const _bindMountUri = await _bindMountPrimaryEndpoint.url();
+const _bindMountConnectionString =
+    await minioBindMount.connectionStringExpression();
 
 await builder.build().run();
