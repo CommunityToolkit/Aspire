21fe96c38d1befb6921ededf999ceaf18092044e
Aaron Powell <me@aaron-powell.com>
2026-05-13 15:52:01 +1000

    adding sqlserver to the dab ts apphost demo

--- examples/data-api-builder/CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder.AppHost.TypeScript/apphost.ts
+++ examples/data-api-builder/CommunityToolkit.Aspire.Hosting.Azure.DataApiBuilder.AppHost.TypeScript/apphost.ts
@@ -7,22 +7,36 @@ const appHostDirectory = path.dirname(fileURLToPath(import.meta.url));
 const primaryConfigPath = path.join(appHostDirectory, "dab-config.json");
 const secondaryConfigPath = path.join(appHostDirectory, "dab-config-2.json");
 
-const dab = await builder.addDataAPIBuilder("dab", {
-    configFilePaths: [primaryConfigPath],
-});
-const dabWithOptions = await builder.addDataAPIBuilder("dab-with-options", {
-    configFilePaths: [primaryConfigPath, secondaryConfigPath],
-    httpPort: 5001,
-});
+const sql = await builder
+    .addSqlServer("sql")
+    .withBindMount("./sql-server", "/usr/config")
+    .withBindMount("../database", "/docker-entrypoint-initdb.d")
+    .withEntrypoint("/usr/config/entrypoint.sh");
 
-const _primaryEndpoint = await dab.getEndpoint("http");
-const _host = await _primaryEndpoint.host();
-const _port = await _primaryEndpoint.port();
-const _uri = await _primaryEndpoint.url();
+const db = await sql.addDatabase("trek");
 
-const _secondaryPrimaryEndpoint = await dabWithOptions.getEndpoint("http");
-const _secondaryHost = await _secondaryPrimaryEndpoint.host();
-const _secondaryPort = await _secondaryPrimaryEndpoint.port();
-const _secondaryUri = await _secondaryPrimaryEndpoint.url();
+const dab = await builder
+    .addDataAPIBuilder("dab", {
+        configFilePaths: [primaryConfigPath],
+    })
+    .withReference(db);
+
+const dabWithOptions = await builder
+    .addDataAPIBuilder("dab-with-options", {
+        configFilePaths: [primaryConfigPath, secondaryConfigPath],
+        httpPort: 5001,
+    })
+    .withReference(db);
+
+// const _primaryEndpoint = await dab.getEndpoint("http");
+// // const _host = await _primaryEndpoint.host();
+// const exists = await _primaryEndpoint.exists();
+// const _port = await _primaryEndpoint.port();
+// const _uri = await _primaryEndpoint.url();
+
+// const _secondaryPrimaryEndpoint = await dabWithOptions.getEndpoint("http");
+// const _secondaryHost = await _secondaryPrimaryEndpoint.host();
+// const _secondaryPort = await _secondaryPrimaryEndpoint.port();
+// const _secondaryUri = await _secondaryPrimaryEndpoint.url();
 
 await builder.build().run();
