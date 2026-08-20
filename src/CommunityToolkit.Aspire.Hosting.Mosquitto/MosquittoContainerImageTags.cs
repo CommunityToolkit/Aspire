d602f3224376724ea23cdc96ef5c691aed24db61
Aaron Powell <me@aaron-powell.com>
2026-08-20 16:08:23 +1000

    Since eclipse-mosquitto is an official image it needs a 'library' prefix

--- src/CommunityToolkit.Aspire.Hosting.Mosquitto/MosquittoContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.Mosquitto/MosquittoContainerImageTags.cs
@@ -6,7 +6,7 @@ internal static class MosquittoContainerImageTags
     internal const string Registry = "docker.io";
 
     /// <remarks>eclipse-mosquitto</remarks>
-    internal const string Image = "eclipse-mosquitto";
+    internal const string Image = "library/eclipse-mosquitto";
 
     /// <remarks>2.0.22</remarks>
     internal const string Tag = "2.0.22";
