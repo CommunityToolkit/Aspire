bde6f4ec12cad82280adb187a683ab3d29755b21
Aaron Powell <me@aaron-powell.com>
2026-08-20 15:46:25 +1000

    bumping the container images for ollama/open-webui (#1534)

--- src/CommunityToolkit.Aspire.Hosting.Ollama/OllamaContainerImageTags.cs
+++ src/CommunityToolkit.Aspire.Hosting.Ollama/OllamaContainerImageTags.cs
@@ -4,9 +4,9 @@ internal static class OllamaContainerImageTags
 {
     public const string Registry = "docker.io";
     public const string Image = "ollama/ollama";
-    public const string Tag = "0.13.0";
+    public const string Tag = "0.32.15";
 
     public const string OpenWebUIRegistry = "ghcr.io";
     public const string OpenWebUIImage = "open-webui/open-webui";
-    public const string OpenWebUITag = "0.6.38";
+    public const string OpenWebUITag = "0.11.0";
 }
